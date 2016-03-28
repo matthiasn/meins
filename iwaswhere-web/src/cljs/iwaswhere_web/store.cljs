761a158aadb7ebb2ef295792fc5a2ef7cfde0a6a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-03-28 23:37:07 +0200

    only update store when containing new tags

--- iwaswhere-web/src/cljs/iwaswhere_web/store.cljs
+++ iwaswhere-web/src/cljs/iwaswhere_web/store.cljs
@@ -1,10 +1,14 @@
 (ns iwaswhere-web.store)
 
 (defn entry-save-fn
+  "Save current entry that's being written and update hashtags for entry filter."
   [{:keys [current-state msg-payload]}]
-  {:new-state (assoc-in current-state [:new-entry] msg-payload)})
+  (when-not (= (:tags (:new-entry current-state)) (:tags msg-payload))
+    {:new-state (assoc-in current-state [:new-entry] msg-payload)}))
 
 (defn new-state-fn
+  "Update client side state with state received from backend. Currently contains
+  all entries."
   [{:keys [current-state msg-payload]}]
   {:new-state (assoc-in current-state [:entries] (:entries msg-payload))})
 
