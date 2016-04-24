ddadcbc8f89b428043ae18bb0f99feaff957f792
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-04-24 19:30:48 +0900

    more consistent styling

--- iwaswhere-web/src/cljs/iwaswhere_web/new_entry.cljs
+++ iwaswhere-web/src/cljs/iwaswhere_web/new_entry.cljs
@@ -22,22 +22,35 @@
   [{:keys [local put-fn]}]
   [:div.l-box-lrg.pure-g
    [:div.pure-u-1
-    [:div [:textarea#new-entry-textbox
-           {:type      "text"
-            ; TODO: occasionally store content into localstorage
-            :on-change (fn [ev]
-                         (let [prev-tags (or (:prev-tags @local) #{})
-                               new-state (merge (h/parse-entry (.. ev -target -value))
-                                                {:timestamp (st/now)})
-                               new-tags (:tags new-state)]
-                           (swap! local assoc-in [:entry] new-state)
-                           (put-fn [:text-entry/save new-state])
-                           (when (not= prev-tags new-tags)
-                             (put-fn [:state/get {:tags new-tags}])
-                             (swap! local assoc-in [:prev-tags] new-tags))))}]]
+    (let [tags (:tags (:entry @local))]
+      [:div.hashtags
+       (when (seq tags)
+         (for [hashtag tags]
+           ^{:key (str "tag-" hashtag)}
+           [:span.hashtag hashtag]))])
+    [:div.textentry
+     [:textarea#new-entry-textbox
+      {:type      "text"
+       :value     (:md (:entry @local))
+       ; TODO: occasionally store content into localstorage
+       :on-change (fn [ev]
+                    (let [prev-tags (or (:prev-tags @local) #{})
+                          new-state (merge (h/parse-entry (.. ev -target -value))
+                                           {:timestamp (st/now)})
+                          new-tags (:tags new-state)]
+                      (swap! local assoc-in [:entry] new-state)
+                      (put-fn [:text-entry/save new-state])
+                      (when (not= prev-tags new-tags)
+                        (put-fn [:state/get {:tags new-tags}])
+                        (swap! local assoc-in [:prev-tags] new-tags))))}]]
     #_(h/pp-div @local)
+    #_(let [tags (:tags (:entry @local))]
+        (when (seq tags)
+          [:div.pure-u-1.hashtags
+           (for [hashtag tags]
+             ^{:key (str "tag-" hashtag)}
+             [:span.hashtag hashtag])]))
     [:div.entry-footer
-     [:div
       [:button.pure-button.pure-button-primary.button-xsmall
        {:on-click #(let [entry (merge (h/parse-entry "...") {:timestamp (st/now)})]
                     (put-fn [:text-entry/persist entry])
@@ -46,15 +59,12 @@
        {:on-click #(let [entry (:entry @local)]
                     (put-fn [:text-entry/persist entry])
                     (send-w-geolocation entry put-fn))} [:span.fa.fa-floppy-o] " save"]
-      [:button.pure-button.button-xsmall {:on-click #(put-fn [:import/photos])} [:span.fa.fa-camera-retro] " import"]
-      [:button.pure-button.button-xsmall {:on-click #(put-fn [:import/geo])} [:span.fa.fa-map-o ] " import"]
-      [:button.pure-button.button-xsmall {:on-click #(put-fn [:import/phone])} [:span.fa.fa-mobile-phone ] " import"]]
-     (let [tags (:tags (:entry @local))]
-       (when (seq tags)
-         [:div.hashtags
-          (for [hashtag tags]
-            ^{:key (str "tag-" hashtag)}
-            [:span.hashtag hashtag])]))]]])
+      [:button.pure-button.button-xsmall
+       {:on-click #(put-fn [:import/photos])} [:span.fa.fa-camera-retro] " import"]
+      [:button.pure-button.button-xsmall
+       {:on-click #(put-fn [:import/geo])} [:span.fa.fa-map-o] " import"]
+      [:button.pure-button.button-xsmall
+       {:on-click #(put-fn [:import/phone])} [:span.fa.fa-mobile-phone] " import"]]]])
 
 (defn cmp-map
   [cmp-id]
