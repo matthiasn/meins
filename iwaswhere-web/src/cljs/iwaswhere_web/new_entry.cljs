f6183ba1e204abe9d60294446ee781146ebcf19b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-04-26 15:12:39 -0700

    some refactoring

--- iwaswhere-web/src/cljs/iwaswhere_web/new_entry.cljs
+++ iwaswhere-web/src/cljs/iwaswhere_web/new_entry.cljs
@@ -20,54 +20,45 @@
 (defn new-entry-view
   "Renders New Entry component."
   [{:keys [local put-fn]}]
-  [:div.l-box-lrg.pure-g
-   [:div.pure-u-1
-    (let [tags (:tags (:entry @local))]
-      [:div.hashtags
-       (when (seq tags)
-         (for [hashtag tags]
-           ^{:key (str "tag-" hashtag)}
-           [:span.hashtag.float-left hashtag]))])
-    [:div.textentry
-     [:textarea#new-entry-textbox
-      {:type      "text"
-       :value     (:md (:entry @local))
-       ; TODO: occasionally store content into localstorage
-       :on-change (fn [ev]
-                    (let [prev-tags (or (:prev-tags @local) #{})
-                          target (.. ev -target)
-                          new-state (merge (h/parse-entry (.. target -value))
-                                           {:timestamp (st/now)})
-                          cursor-pos (.. target -selectionEnd)
-                          new-tags (:tags new-state)]
-                      (swap! local assoc-in [:entry] new-state)
-                      (put-fn [:new-entry/tmp-save new-state])
-                      (when (not= prev-tags new-tags)
-                        (put-fn [:state/get {:tags new-tags}])
-                        (swap! local assoc-in [:prev-tags] new-tags))))}]]
-    #_(h/pp-div @local)
-    [:div.entry-footer
-      [:button.pure-button.pure-button-primary.button-xsmall
-       {:on-click #(let [ts (st/now)
-                         entry (merge (h/parse-entry "...") {:timestamp ts})]
-                    (put-fn [:text-entry/persist entry])
-                    (put-fn [:cmd/toggle {:timestamp ts :key :show-edit-for}])
-                    (send-w-geolocation entry put-fn))}
-       [:span.fa.fa-plus-square] " new"]
-      [:button.pure-button.pure-button-primary.button-xsmall
-       {:on-click #(let [entry (:entry @local)]
-                    (put-fn [:text-entry/persist entry])
-                    (send-w-geolocation entry put-fn))}
-       [:span.fa.fa-floppy-o] " save"]
-      [:button.pure-button.button-xsmall
-       {:on-click #(put-fn [:import/photos])}
-       [:span.fa.fa-camera-retro] " import"]
-      [:button.pure-button.button-xsmall
-       {:on-click #(put-fn [:import/geo])}
-       [:span.fa.fa-map-o] " import"]
-      [:button.pure-button.button-xsmall
-       {:on-click #(put-fn [:import/phone])}
-       [:span.fa.fa-mobile-phone] " import"]]]])
+  (let [on-change-fn #(let [prev-tags (or (:prev-tags @local) #{})
+                            new-state (merge (h/parse-entry (.. % -target -value)) {:timestamp (st/now)})
+                            new-tags (:tags new-state)]
+                       (swap! local assoc-in [:entry] new-state)
+                       (put-fn [:new-entry/tmp-save new-state])
+                       (when (not= prev-tags new-tags)
+                         (put-fn [:state/get {:tags new-tags}])
+                         (swap! local assoc-in [:prev-tags] new-tags)))
+        new-entry-fn #(let [ts (st/now)
+                            entry (merge (h/parse-entry "...") {:timestamp ts})]
+                       (put-fn [:text-entry/persist entry])
+                       (put-fn [:cmd/toggle {:timestamp ts :key :show-edit-for}])
+                       (send-w-geolocation entry put-fn))
+        save-entry-fn #(let [entry (:entry @local)]
+                        (put-fn [:text-entry/persist entry])
+                        (send-w-geolocation entry put-fn))]
+    [:div.l-box-lrg.pure-g
+     [:div.pure-u-1
+      (let [tags (:tags (:entry @local))]
+        [:div.hashtags
+         (when (seq tags)
+           (for [hashtag tags]
+             ^{:key (str "tag-" hashtag)}
+             [:span.hashtag.float-left hashtag]))])
+      [:div.textentry
+       [:textarea#new-entry-textbox {:type      "text"
+                                     :value     (:md (:entry @local))
+                                     :on-change on-change-fn}]]
+      [:div.entry-footer
+       [:button.pure-button.pure-button-primary.button-xsmall {:on-click new-entry-fn}
+        [:span.fa.fa-plus-square] " new"]
+       [:button.pure-button.pure-button-primary.button-xsmall {:on-click save-entry-fn}
+        [:span.fa.fa-floppy-o] " save"]
+       [:button.pure-button.button-xsmall {:on-click #(put-fn [:import/photos])}
+        [:span.fa.fa-camera-retro] " import"]
+       [:button.pure-button.button-xsmall {:on-click #(put-fn [:import/geo])}
+        [:span.fa.fa-map-o] " import"]
+       [:button.pure-button.button-xsmall {:on-click #(put-fn [:import/phone])}
+        [:span.fa.fa-mobile-phone] " import"]]]]))
 
 (defn cmp-map
   [cmp-id]
