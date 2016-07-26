e55b2b4d91c20f09dd72fd73823b09b9cca7e75c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-07-26 17:20:44 +0200

    only show entry-related action buttons when required

--- iwaswhere-web/src/cljs/iwaswhere_web/ui/entry.cljs
+++ iwaswhere-web/src/cljs/iwaswhere_web/ui/entry.cljs
@@ -24,9 +24,9 @@
        [:span.hashtag hashtag])]))
 
 (defn trash-icon
-  "Renders a trash icon, which transforms into a warning button that needs to be clicked
-   again for actual deletion. This label is a little to the right, so it can't be clicked
-   accidentally, and disappears again within 5 seconds."
+  "Renders a trash icon, which transforms into a warning button that needs to be
+   clicked again for actual deletion. This label is a little to the right, so it
+   can't be clicked accidentally, and disappears again within 5 seconds."
   [trash-fn]
   (let [clicked (r/atom false)
         guarded-trash-fn (fn [_ev]
@@ -34,18 +34,20 @@
                            (.setTimeout js/window #(reset! clicked false) 5000))]
     (fn [trash-fn]
       (if @clicked
-        [:span.delete-warn {:on-click trash-fn} [:span.fa.fa-trash] "  confirm delete?"]
+        [:span.delete-warn {:on-click trash-fn}
+         [:span.fa.fa-trash] "  confirm delete?"]
         [:span.fa.fa-trash-o.toggle {:on-click guarded-trash-fn}]))))
 
 (defn edit-icon
-  "Renders an edit icon, which transforms into a warning button that needs to be clicked
-   again for actually discarding changes. This label is a little to the right, so it can't be
-   clicked accidentally, and disappears again within 5 seconds."
+  "Renders an edit icon, which transforms into a warning button that needs to be
+   clicked again for actually discarding changes. This label is a little to the
+   right, so it can't be clicked accidentally, and disappears again within 5
+   seconds."
   [toggle-edit edit-mode?]
   (let [clicked (r/atom false)
         guarded-edit-fn (fn [_ev]
-                           (swap! clicked not)
-                           (.setTimeout js/window #(reset! clicked false) 5000))]
+                          (swap! clicked not)
+                          (.setTimeout js/window #(reset! clicked false) 5000))]
     (fn [toggle-edit edit-mode?]
       (if edit-mode?
         (if @clicked
@@ -57,59 +59,107 @@
 (defn new-link
   "Renders input for adding link entry."
   [entry put-fn create-linked-entry]
-  (let [visible (r/atom false)]
+  (let [visible (r/atom false)
+        keydown-fn
+        (fn [ev]
+          (when (= (.-keyCode ev) 13)
+            (let [link (re-find #"[0-9]{13}" (.-value (.-target ev)))
+                  entry-links (:linked-entries entry)
+                  linked-entries (conj entry-links (long link))
+                  new-entry (h/clean-entry
+                              (merge entry {:linked-entries linked-entries}))]
+              (when link
+                (put-fn [:entry/update new-entry])
+                (swap! visible not)))))]
     (fn [entry put-fn create-linked-entry]
       [:span.new-link-btn
        [:span.fa.fa-link.toggle {:on-click #(swap! visible not)}]
        (when @visible
          [:span.new-link
-          [:span.fa.fa-plus-square {:on-click #(do (create-linked-entry) (swap! visible not))}]
+          [:span.fa.fa-plus-square
+           {:on-click #(do (create-linked-entry) (swap! visible not))}]
           [:input {:on-click    #(.stopPropagation %)
-                   :on-key-down (fn [ev]
-                                  (when (= (.-keyCode ev) 13)
-                                    (let [link (re-find #"[0-9]{13}" (.-value (.-target ev)))
-                                          entry-links (:linked-entries entry)
-                                          linked-entries (conj entry-links (long link))
-                                          new-entry (h/clean-entry
-                                                      (merge entry
-                                                             {:linked-entries linked-entries}))]
-                                      (when link
-                                        (put-fn [:entry/update new-entry])
-                                        (swap! visible not)))))}]])])))
+                   :on-key-down keydown-fn}]])])))
+
+(defn entry-actions
+  "Entry-related action buttons. Hidden by default, become visible when mouse
+   hovers over element, stays visible for a little while after mose leaves."
+  [entry cfg put-fn edit-mode?]
+  (let [visible (r/atom false)
+        hide-fn (fn [_ev]
+                  (.setTimeout js/window #(reset! visible false) 6000))]
+    (fn
+      [entry cfg put-fn edit-mode?]
+      (let [ts (:timestamp entry)
+            map? (:latitude entry)
+            toggle-map #(put-fn [:cmd/toggle
+                                 {:timestamp ts
+                                  :path      [:cfg :show-maps-for]}])
+            show-comments? (contains? (:show-comments-for cfg) ts)
+            toggle-comments #(put-fn [:cmd/toggle
+                                      {:timestamp ts
+                                       :path      [:cfg :show-comments-for]}])
+            create-comment (h/new-entry-fn put-fn {:comment-for ts})
+            create-linked-entry (h/new-entry-fn put-fn {:linked-entries [ts]})
+            new-pomodoro #(do ((h/new-entry-fn put-fn (p/pomodoro-defaults ts)))
+                              (put-fn [:cmd/set-opt
+                                       {:timestamp ts
+                                        :path      [:cfg :show-comments-for]}]))
+            toggle-edit #(if edit-mode? (put-fn [:entry/remove-local entry])
+                                        (put-fn [:entry/update-local entry]))
+            trash-entry #(if edit-mode?
+                          (put-fn [:entry/remove-local {:timestamp ts}])
+                          (put-fn [:entry/trash {:timestamp ts}]))
+            upvotes (:upvotes entry)
+            upvote-fn (fn [op]
+                        #(put-fn [:entry/update
+                                  (update-in entry [:upvotes] op)]))]
+        [:div {:on-mouse-enter #(reset! visible true)
+               :on-mouse-leave hide-fn
+               :style          {:opacity (if @visible 1 0)}}
+         [:span.fa.toggle
+          {:on-click (upvote-fn inc)
+           :class    (if (pos? upvotes) "fa-thumbs-up" "fa-thumbs-o-up")}]
+         (when (pos? upvotes) [:span.upvotes " " upvotes])
+         (when (pos? upvotes)
+           [:span.fa.fa-thumbs-down.toggle {:on-click (upvote-fn dec)}])
+         (when map? [:span.fa.fa-map-o.toggle {:on-click toggle-map}])
+         [edit-icon toggle-edit edit-mode?]
+         (when-not (:comment-for entry)
+           [:span.fa.fa-clock-o.toggle {:on-click new-pomodoro}])
+         (when-not (:comment-for entry)
+           [:span.fa.fa-comment-o.toggle {:on-click create-comment}])
+         (when (seq (:comments entry))
+           [:span.fa.fa-comments.toggle
+            {:on-click toggle-comments
+             :class    (when-not show-comments? "hidden-comments")}])
+         (when-not (:comment-for entry)
+           [:a {:href (str "/#" ts) :target "_blank"}
+            [:span.fa.fa-external-link.toggle]])
+         (when-not (:comment-for entry)
+           [new-link entry put-fn create-linked-entry])
+         [trash-icon trash-entry]]))))
 
 (defn journal-entry
-  "Renders individual journal entry. Interaction with application state happens via
-  messages that are sent to the store component, for example for toggling the display
-  of the edit mode or showing the map for an entry. The editable content component
-  used in edit mode also sends a modified entry to the store component, which is useful
-  for displaying updated hashtags, or also for showing the warning that the entry is not
-  saved yet."
+  "Renders individual journal entry. Interaction with application state happens
+   via messages that are sent to the store component, for example for toggling
+   the display of the edit mode or showing the map for an entry. The editable
+   content component used in edit mode also sends a modified entry to the store
+   component, which is useful for displaying updated hashtags, or also for
+   showing the warning that the entry is not saved yet."
   [entry cfg put-fn edit-mode? info]
   (let [ts (:timestamp entry)
-        map? (:latitude entry)
         show-map? (contains? (:show-maps-for cfg) ts)
-        toggle-map #(put-fn [:cmd/toggle {:timestamp ts :path [:cfg :show-maps-for]}])
-        show-comments? (contains? (:show-comments-for cfg) ts)
-        toggle-comments #(put-fn [:cmd/toggle {:timestamp ts :path [:cfg :show-comments-for]}])
-        create-comment (h/new-entry-fn put-fn {:comment-for ts})
-        create-linked-entry (h/new-entry-fn put-fn {:linked-entries [ts]})
-        create-pomodoro #(do ((h/new-entry-fn put-fn (p/pomodoro-defaults ts)))
-                             (put-fn [:cmd/set-opt {:timestamp ts
-                                                    :path      [:cfg :show-comments-for]}]))
         toggle-edit #(if edit-mode? (put-fn [:entry/remove-local entry])
                                     (put-fn [:entry/update-local entry]))
-        trash-entry #(if edit-mode? (put-fn [:entry/remove-local {:timestamp ts}])
-                                    (put-fn [:entry/trash {:timestamp ts}]))
-        upvotes (:upvotes entry)
-        upvote-fn (fn [op] #(put-fn [:entry/update (update-in entry [:upvotes] op)]))
         hashtags (:hashtags cfg)
         mentions (:mentions cfg)
         arrival-ts (:arrival-timestamp entry)
         depart-ts (:departure-timestamp entry)
-        dur (when (and arrival-ts depart-ts) (-> (- depart-ts arrival-ts)
-                                                 (/ 1000)
-                                                 (Math/floor)))
-        formatted-duration (when (and dur (< dur 99999)) (str ", " (u/duration-string dur)))]
+        dur (when (and arrival-ts depart-ts)
+              (-> (- depart-ts arrival-ts) (/ 1000) (Math/floor)))
+        formatted-duration (when (and dur (< dur 99999))
+                             (str ", " (u/duration-string dur)))]
     [:div.entry
      [:div.header
       [:div
@@ -123,40 +173,26 @@
        (when (seq (:linked-entries-list entry))
          (let [entry-active? (= (:active cfg) (:timestamp entry))
                set-active-fn #(put-fn [:cmd/toggle-active (:timestamp entry)])]
-           [:span.link-btn {:on-click set-active-fn :class (when entry-active? "active")}
+           [:span.link-btn {:on-click set-active-fn
+                            :class    (when entry-active? "active")}
             (str " linked: " (count (:linked-entries-list entry)))]))]
-      [:div
-       [:span.fa.toggle
-        {:on-click (upvote-fn inc) :class (if (pos? upvotes) "fa-thumbs-up" "fa-thumbs-o-up")}]
-       (when (pos? upvotes) [:span.upvotes " " upvotes])
-       (when (pos? upvotes) [:span.fa.fa-thumbs-down.toggle {:on-click (upvote-fn dec)}])
-       (when map? [:span.fa.fa-map-o.toggle {:on-click toggle-map}])
-       ;[:span.fa.fa-pencil-square-o.toggle {:on-click toggle-edit}]
-       [edit-icon toggle-edit edit-mode?]
-       (when-not (:comment-for entry) [:span.fa.fa-clock-o.toggle {:on-click create-pomodoro}])
-       (when-not (:comment-for entry) [:span.fa.fa-comment-o.toggle {:on-click create-comment}])
-       (when (seq (:comments entry))
-         [:span.fa.fa-comments.toggle {:on-click toggle-comments
-                                       :class    (when-not show-comments? "hidden-comments")}])
-       (when-not (:comment-for entry)
-         [:a {:href (str "/#" ts) :target "_blank"} [:span.fa.fa-external-link.toggle]])
-       (when-not (:comment-for entry) [new-link entry put-fn create-linked-entry])
-       [trash-icon trash-entry]]]
+      [entry-actions entry cfg put-fn edit-mode?]]
      [hashtags-mentions-list entry]
      [l/leaflet-map entry (or show-map? (:show-all-maps cfg))]
-     (if edit-mode? [e/editable-md-render entry hashtags mentions put-fn toggle-edit]
-                    [md/markdown-render entry (:show-hashtags cfg)])
+     (if edit-mode?
+       [e/editable-md-render entry hashtags mentions put-fn toggle-edit]
+       [md/markdown-render entry (:show-hashtags cfg)])
      [m/image-view entry]
      [m/audioplayer-view entry]
      [m/videoplayer-view entry]]))
 
 (defn entry-with-comments
-  "Renders individual journal entry. Interaction with application state happens via
-  messages that are sent to the store component, for example for toggling the display
-  of the edit mode or showing the map for an entry. The editable content component
-  used in edit mode also sends a modified entry to the store component, which is useful
-  for displaying updated hashtags, or also for showing the warning that the entry is not
-  saved yet."
+  "Renders individual journal entry. Interaction with application state happens
+   via messages that are sent to the store component, for example for toggling
+   the display of the edit mode or showing the map for an entry. The editable
+   content component used in edit mode also sends a modified entry to the store
+   component, which is useful for displaying updated hashtags, or also for
+   showing the warning that the entry is not saved yet."
   [entry cfg new-entries put-fn]
   (let [ts (:timestamp entry)
         entry (or (get new-entries ts) entry)
