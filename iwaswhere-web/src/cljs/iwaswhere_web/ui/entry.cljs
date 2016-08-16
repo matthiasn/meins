bddffd2909d3e314b160f601b5785c52722482a5
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-08-16 14:49:12 +0200

    show comments on button hover

--- iwaswhere-web/src/cljs/iwaswhere_web/ui/entry.cljs
+++ iwaswhere-web/src/cljs/iwaswhere_web/ui/entry.cljs
@@ -295,6 +295,7 @@
             ^{:key (str "c" (:timestamp comment))}
             [journal-entry comment cfg put-fn
              (contains? new-entries (:timestamp comment))])]
-         [:div.show-comments {:on-click toggle-comments}
+         [:div.show-comments {:on-click       toggle-comments
+                              :on-mouse-enter toggle-comments}
           (let [n (count comments)]
             [:span (str "show " n " comment" (when (> n 1) "s"))])]))]))
