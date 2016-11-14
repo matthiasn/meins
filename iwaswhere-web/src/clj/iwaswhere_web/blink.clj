658353d30679b48940e8ade2531e663d05b37e7d
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-11-14 20:41:52 +0100

    change blink brightness depending on time of day

--- iwaswhere-web/src/clj/iwaswhere_web/blink.clj
+++ iwaswhere-web/src/clj/iwaswhere_web/blink.clj
@@ -5,10 +5,12 @@
   (:require [matthiasn.systems-toolbox.component :as st]
             [clojure.java.shell :refer [sh]]
             [me.raynes.fs :as fs]
+            [clj-time.core :as ct]
+            [clj-time.local :as ctl]
             [clojure.tools.logging :as log]))
 
-(def red "--hsb=265,255,100")
-(def green "--hsb=65,255,100")
+(def red {:day "--hsb=265,255,255" :night "--hsb=265,255,100"})
+(def green {:day "--hsb=65,255,255" :night "--hsb=65,255,100"})
 
 (defn blink
   "Calls the blink1 command line tool, which can for example be installed via
@@ -28,22 +30,29 @@
                                 :repeat  true}])
     {:state state}))
 
+(defn day-night?
+  []
+  (let [h (ct/hour (ctl/local-now))]
+    (if (or (< h 8) (> h 18)) :night :day)))
+
 (defn blink-fn
   "Called with each heartbeat. Sets color to green when last busy timestamp is
    longer ago than one second."
   [{:keys [current-state]}]
   (let [now (st/now)
+        k (day-night?)
         last-busy (get-in current-state [:last-busy])]
     (when (> (- now last-busy) 1000)
       (log/debug "blink green")
-      (blink green))))
+      (blink (k green)))))
 
 (defn blink-busy
   "Set light to red when busy and save last busy timestamp."
   [{:keys [current-state]}]
-  (let [ts (st/now)]
+  (let [ts (st/now)
+        k (day-night?)]
     (log/debug "blink red")
-    (blink red)
+    (blink (k red))
     {:new-state (assoc-in current-state [:last-busy] ts)}))
 
 (defn cmp-map
