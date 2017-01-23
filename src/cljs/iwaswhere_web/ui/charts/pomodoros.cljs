81c3ab6cf0e8aec54d3845d15cedc256b2fe579a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-01-23 18:56:39 +0100

    basic chart for time spent by story

--- src/cljs/iwaswhere_web/ui/charts/pomodoros.cljs
+++ src/cljs/iwaswhere_web/ui/charts/pomodoros.cljs
@@ -23,6 +23,48 @@
                :on-mouse-enter mouse-enter-fn
                :on-mouse-leave mouse-leave-fn}]))])
 
+(defn day-bars
+  "Renders group with rects for all stories of the particular day."
+  [day-stats local idx chart-h y-scale put-fn]
+  (let [options (subscribe [:options])
+        stories (reaction (:stories @options))
+        stacked-reducer (fn [acc [k v]]
+                          (let [total (get acc :total 0)]
+                            (-> acc
+                                (assoc-in [:total] (+ total v))
+                                (assoc-in [:items k] (+ total v)))))]
+    (fn [day-stats local idx chart-h y-scale put-fn]
+      (let [mouse-enter-fn (cc/mouse-enter-fn local day-stats)
+            mouse-leave-fn (cc/mouse-leave-fn local day-stats)
+            stories @stories
+            time-by-story (sort-by #(str (first %)) (:time-by-story day-stats))
+            stacked (reduce stacked-reducer {} time-by-story)
+            time-by-story2 (reverse (sort-by #(str (first %)) (:items stacked)))]
+        [:g
+         {:on-mouse-enter mouse-enter-fn
+          :on-mouse-leave mouse-leave-fn}
+         (for [[story v] time-by-story2]
+           (let [h (* y-scale v)
+                 story-name (or (:story-name (get stories story)) "No story")]
+             ^{:key (str story)}
+             [:rect {:on-click (cc/open-day-fn v put-fn)
+                     :fill     (cc/item-color story-name)
+                     :x        (* 20 idx)
+                     :y        (- chart-h h)
+                     :width    20
+                     :height   h}]))]))))
+
+(defn bars-by-story
+  "Renders chart with daily recorded times, split up by story."
+  [indexed local chart-h y-scale put-fn]
+  [:g
+   (for [[idx v] indexed]
+     (let [h (* y-scale (:total-time v))
+           mouse-enter-fn (cc/mouse-enter-fn local v)
+           mouse-leave-fn (cc/mouse-leave-fn local v)]
+       ^{:key (str idx)}
+       [day-bars v local idx chart-h y-scale put-fn]))])
+
 (defn time-by-stories-list
   "Render list of times spent on individual stories, plus the total."
   [day-stats]
@@ -48,18 +90,18 @@
 
 (defn pomodoro-bar-chart
   [pomodoro-stats chart-h title y-scale put-fn]
-  (let [local (rc/atom {})]
+  (let [local (rc/atom {})
+        idx-fn (fn [idx [k v]] [idx v])]
     (fn [pomodoro-stats chart-h title y-scale put-fn]
-      (let [indexed (map-indexed (fn [idx [k v]] [idx v]) pomodoro-stats)]
+      (let [indexed (map-indexed idx-fn pomodoro-stats)
+            indexed-30 (map-indexed idx-fn (take-last 30 pomodoro-stats))]
         [:div
          [:svg
           {:viewBox (str "0 0 600 " chart-h)}
           [:g
            [cc/chart-title "Time tracked"]
            [cc/bg-bars indexed local chart-h :pomodoro]
-           [bars indexed local :total-time chart-h 0.0025 put-fn]
-           [cc/path "M 0 50 l 600 0 z"]
-           [cc/path "M 0 100 l 600 0 z"]]]
+           [bars-by-story indexed-30 local chart-h 0.0035 put-fn]]]
          (if-let [mouse-over (:mouse-over @local)]
            [time-by-stories-list mouse-over]
            [time-by-stories-list (second (last pomodoro-stats))])
