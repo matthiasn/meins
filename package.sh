ac9ac9fb0b9c24916bfdacdd8d8d7d195a04c0e9
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-08-17 22:24:10 +0200

    compression settings

--- package.sh
+++ package.sh
@@ -2,6 +2,8 @@
 
 cd electron
 
+export ELECTRON_BUILDER_COMPRESSION_LEVEL=3
+
 DEBUG=electron-builder,electron-builder:* electron-builder --publish always -m zip
 DEBUG=electron-builder,electron-builder:* electron-builder --publish always -w nsis
 #DEBUG=electron-builder,electron-builder:* electron-builder --publish always -l AppImage
