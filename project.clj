45b2393c0bab73f2da49f6a83b82bc8af3496d24
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-09-01 21:50:26 +0200

    latest systems-toolbox: avoid zombie processes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/iwaswhere-web "0.2.95"
+(defproject matthiasn/iwaswhere-web "0.2.106"
   :description "Sample application built with systems-toolbox library"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE"
@@ -23,7 +23,7 @@
                  [factual/geo "1.1.0"]
                  [camel-snake-kebab "0.4.0"]
                  [matthiasn/systems-toolbox-kafka "0.6.13"]
-                 [matthiasn/systems-toolbox "0.6.11"]
+                 [matthiasn/systems-toolbox "0.6.12"]
                  [matthiasn/systems-toolbox-sente "0.6.16"]
                  [matthiasn/systems-toolbox-zipkin "0.6.3"]
                  [reagent "0.7.0" :exclusions [cljsjs/react cljsjs/react-dom]]
