da076630ae0d840ba36a06203639b9e1cc2c6653
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-10-29 23:06:10 +0100

    ui pulled out

--- mobile/src/cljs/i_was_where_app/ios/core.cljs
+++ mobile/src/cljs/i_was_where_app/ios/core.cljs
@@ -14,18 +14,6 @@
             [matthiasn.systems-toolbox.component :as st]
             [clojure.pprint :as pp]))
 
-(def ReactNative (js/require "react-native"))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def text-input (r/adapt-react-class (.-TextInput ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
-
-(def logo-img (js/require "./images/icon.png"))
-(def react-native-camera (js/require "react-native-camera"))
-(def cam (r/adapt-react-class react-native-camera))
-(def device-info (js/require "react-native-device-info"))
-
 (defonce switchboard (sb/component :client/switchboard))
 
 (def OBSERVER true)
@@ -33,7 +21,7 @@
 (defn make-observable [components]
   (if OBSERVER
     (let [mapper #(assoc-in % [:opts :msgs-on-firehose] true)]
-      (prn "Attaching firehose")
+      (println "CORE: Attaching firehose")
       (set (mapv mapper components)))
     components))
 
@@ -46,109 +34,6 @@
                                :state/search :cfg/refresh :firehose/cmp-recv
                                :firehose/cmp-put}
                 :sente-opts  {:host "172.20.10.2:8765"}})
-
-(defn app-root []
-  (let [entries (subscribe [:entries])
-        stats (subscribe [:stats])
-        local (r/atom {:md "hello world"})
-        device-id (.getUniqueID device-info)]
-    (fn []
-      [view {:style {:flex-direction   "column"
-                     :padding-top      30
-                     :padding-bottom   30
-                     :padding-left     20
-                     :padding-right    20
-                     :height           "100%"
-                     :background-color "#222"
-                     :align-items      "center"}}
-       [text {:style {:font-size     10
-                      :color         :white
-                      :font-weight   "100"
-                      :margin-bottom 5
-                      :text-align    "center"}}
-        (str device-id " - " (count @entries) " entries")]
-       ;[cam {}]
-       [text-input {:style          {:height           200
-                                     :font-weight      "100"
-                                     :padding          10
-                                     :font-size        20
-                                     :background-color "#CCC"
-                                     :width            "100%"}
-                    :multiline      true
-                    :default-value  (:md @local)
-                    :keyboard-type  "twitter"
-                    :on-change-text (fn [text]
-                                      (swap! local assoc-in [:md] text))}]
-       [view {:style {:flex-direction "row"
-                      :padding-top    10
-                      :padding-bottom 10
-                      :padding-left   20
-                      :padding-right  20}}
-        [touchable-highlight
-         {:style    {:background-color "green"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12
-                     :margin-right     20}
-          :on-press #(let [put-fn @ui/put-fn-atom
-                           new-entry (p/parse-entry (:md @local))
-                           new-entry-fn (h/new-entry-fn put-fn new-entry nil)]
-                       (new-entry-fn)
-                       (swap! local assoc-in [:md] ""))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "new"]]
-        [touchable-highlight
-         {:style    {:background-color "blue"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12
-                     :margin-right     20}
-          :on-press #(let [put-fn @ui/put-fn-atom]
-                       (dotimes [n 30]
-                         (put-fn [:healthkit/steps n])))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "steps"]]
-        [touchable-highlight
-         {:style    {:background-color "blue"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12
-                     :margin-right     20}
-          :on-press #(let [put-fn @ui/put-fn-atom]
-                       (put-fn [:healthkit/weight]))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "weight"]]
-        [touchable-highlight
-         {:style    {:background-color "#999"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12}
-          :on-press #(let [put-fn @ui/put-fn-atom])}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "cam"]]]
-
-       [text {:style {:font-size     10
-                      :font-weight   "500"
-                      :color         "#CCC"
-                      :margin-bottom 20
-                      :text-align    "center"}}
-        (with-out-str (pp/pprint (second (last (sort-by first @entries)))))]
-       [image {:source logo-img
-               :style  {:width         80
-                        :height        80
-                        :margin-bottom 5}}]])))
 
 (defn init []
   (dispatch-sync [:initialize-db])
--- mobile/src/cljs/i_was_where_app/ui.cljs
+++ mobile/src/cljs/i_was_where_app/ui.cljs
@@ -1,7 +1,10 @@
 (ns i-was-where-app.ui
   (:require [reagent.core :as r]
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-            [re-frame.db :as rdb]))
+            [re-frame.db :as rdb]
+            [cljs.pprint :as pp]
+            [i-was-where-app.helpers :as h]
+            [iwaswhere-web.utils.parse :as p]))
 
 (defonce put-fn-atom (r/atom nil))
 
@@ -12,11 +15,116 @@
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def image (r/adapt-react-class (.-Image ReactNative)))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
-(def logo-img (js/require "./images/cljs.png"))
+(def logo-img (js/require "./images/icon.png"))
+(def text-input (r/adapt-react-class (.-TextInput ReactNative)))
+
+(def react-native-camera (js/require "react-native-camera"))
+(def cam (r/adapt-react-class react-native-camera))
 
 (defn alert [title]
   (.alert (.-Alert ReactNative) title))
 
+(defn app-root []
+  (let [entries (subscribe [:entries])
+        stats (subscribe [:stats])
+        local (r/atom {:md "hello world"})]
+    (fn []
+      [view {:style {:flex-direction   "column"
+                     :padding-top      30
+                     :padding-bottom   30
+                     :padding-left     20
+                     :padding-right    20
+                     :height           "100%"
+                     :background-color "#222"
+                     :align-items      "center"}}
+       [text {:style {:font-size     10
+                      :color         :white
+                      :font-weight   "100"
+                      :margin-bottom 5
+                      :text-align    "center"}}
+        (str (count @entries) " entries")]
+       ;[cam {}]
+       [text-input {:style          {:height           200
+                                     :font-weight      "100"
+                                     :padding          10
+                                     :font-size        20
+                                     :background-color "#CCC"
+                                     :width            "100%"}
+                    :multiline      true
+                    :default-value  (:md @local)
+                    :keyboard-type  "twitter"
+                    :on-change-text (fn [text]
+                                      (swap! local assoc-in [:md] text))}]
+       [view {:style {:flex-direction "row"
+                      :padding-top    10
+                      :padding-bottom 10
+                      :padding-left   20
+                      :padding-right  20}}
+        [touchable-highlight
+         {:style    {:background-color "green"
+                     :padding-left     20
+                     :padding-right    20
+                     :padding-top      12
+                     :padding-bottom   12
+                     :margin-right     20}
+          :on-press #(let [put-fn @put-fn-atom
+                           new-entry (p/parse-entry (:md @local))
+                           new-entry-fn (h/new-entry-fn put-fn new-entry nil)]
+                       (new-entry-fn)
+                       (swap! local assoc-in [:md] ""))}
+         [text {:style {:color       "white"
+                        :text-align  "center"
+                        :font-weight "bold"}}
+          "new"]]
+        [touchable-highlight
+         {:style    {:background-color "blue"
+                     :padding-left     20
+                     :padding-right    20
+                     :padding-top      12
+                     :padding-bottom   12
+                     :margin-right     20}
+          :on-press #(let [put-fn @put-fn-atom]
+                       (dotimes [n 30]
+                         (put-fn [:healthkit/steps n])))}
+         [text {:style {:color       "white"
+                        :text-align  "center"
+                        :font-weight "bold"}}
+          "steps"]]
+        [touchable-highlight
+         {:style    {:background-color "blue"
+                     :padding-left     20
+                     :padding-right    20
+                     :padding-top      12
+                     :padding-bottom   12
+                     :margin-right     20}
+          :on-press #(let [put-fn @put-fn-atom]
+                       (put-fn [:healthkit/weight]))}
+         [text {:style {:color       "white"
+                        :text-align  "center"
+                        :font-weight "bold"}}
+          "weight"]]
+        [touchable-highlight
+         {:style    {:background-color "#999"
+                     :padding-left     20
+                     :padding-right    20
+                     :padding-top      12
+                     :padding-bottom   12}
+          :on-press #(let [put-fn @put-fn-atom])}
+         [text {:style {:color       "white"
+                        :text-align  "center"
+                        :font-weight "bold"}}
+          "cam"]]]
+
+       [text {:style {:font-size     10
+                      :font-weight   "500"
+                      :color         "#CCC"
+                      :margin-bottom 20
+                      :text-align    "center"}}
+        (with-out-str (pp/pprint (second (last (sort-by first @entries)))))]
+       [image {:source logo-img
+               :style  {:width         80
+                        :height        80
+                        :margin-bottom 5}}]])))
 
 (defn state-fn [put-fn]
   (let [
