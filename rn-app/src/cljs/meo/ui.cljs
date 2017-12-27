5a4ea771731bfb1ca4689fd96ff44d69eb33b7d4
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-12-27 04:47:53 +0100

    styling

--- rn-app/src/cljs/meo/ui.cljs
+++ rn-app/src/cljs/meo/ui.cljs
@@ -14,7 +14,10 @@
 
 (def app-registry (.-AppRegistry ReactNative))
 (def text (r/adapt-react-class (.-Text ReactNative)))
+(def tab-bar (r/adapt-react-class (.-TabBarIOS ReactNative)))
+(def tab-bar-item (r/adapt-react-class (.-Item (.-TabBarIOS ReactNative))))
 (def view (r/adapt-react-class (.-View ReactNative)))
+(def keyboard-avoiding-view (r/adapt-react-class (.-KeyboardAvoidingView ReactNative)))
 (def image (r/adapt-react-class (.-Image ReactNative)))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 ;(def logo-img (js/require "./images/icon.png"))
@@ -27,10 +30,66 @@
 
 (reg-sub :entries (fn [db _] (:entries db)))
 
+(defn menu-bar [local]
+  (let [defaults {:background-color "blue"
+                  :padding-left     15
+                  :padding-right    15
+                  :padding-top      10
+                  :padding-bottom   10
+                  :margin-right     10}]
+    [view {:style {:flex-direction "row"
+                   :padding-top    10
+                   :padding-bottom 10
+                   :padding-left   10
+                   :padding-right  10}}
+     [touchable-highlight
+      {:style    (merge defaults {:background-color "green"})
+       :on-press #(let [put-fn @put-fn-atom
+                        new-entry (p/parse-entry (:md @local))
+                        new-entry-fn (h/new-entry-fn put-fn new-entry nil)]
+                    (new-entry-fn)
+                    (swap! local assoc-in [:md] ""))}
+      [text {:style {:color       "white"
+                     :text-align  "center"
+                     :font-weight "bold"}}
+       "new"]]
+     [touchable-highlight
+      {:style    defaults
+       :on-press #(swap! local update-in [:cam] not)}
+      [text {:style {:color       "white"
+                     :text-align  "center"
+                     :font-weight "bold"}}
+       (if (:cam @local) "hide cam" "ws")]]
+     [touchable-highlight
+      {:style    defaults
+       :on-press #(let [put-fn @put-fn-atom]
+                    (dotimes [n 75]
+                      (put-fn [:healthkit/steps n])))}
+      [text {:style {:color       "white"
+                     :text-align  "center"
+                     :font-weight "bold"}}
+       "steps"]]
+     [touchable-highlight
+      {:style    defaults
+       :on-press #(let [put-fn @put-fn-atom]
+                    (put-fn [:healthkit/weight]))}
+      [text {:style {:color       "white"
+                     :text-align  "center"
+                     :font-weight "bold"}}
+       "weight"]]
+     [touchable-highlight
+      {:style    defaults
+       :on-press #(let [put-fn @put-fn-atom]
+                    (put-fn [:healthkit/bp]))}
+      [text {:style {:color       "white"
+                     :text-align  "center"
+                     :font-weight "bold"}}
+       "bp"]]]))
+
 (defn app-root [put-fn]
   (let [entries (subscribe [:entries])
         local (r/atom {:cam false
-                       :md "hello world"})
+                       :md  "hello world"})
         on-barcode-read (fn [e]
                           (let [qr-code (js->clj e)
                                 data (get qr-code "data")]
@@ -38,14 +97,16 @@
                             (put-fn [:ws/connect {:host data}])
                             (swap! local assoc-in [:cam] false)))]
     (fn []
-      [view {:style {:flex-direction   "column"
-                     :padding-top      30
-                     :padding-bottom   30
-                     :padding-left     20
-                     :padding-right    20
-                     :height           "100%"
-                     :background-color "#222"
-                     :align-items      "center"}}
+      [keyboard-avoiding-view
+       {:keyboardVerticalOffset 0
+        :style                  {:flex-direction   "column"
+                                 :padding-top      30
+                                 :padding-bottom   30
+                                 :padding-left     20
+                                 :padding-right    20
+                                 :height           "100%"
+                                 :background-color "#777"
+                                 :align-items      "center"}}
        [text {:style {:font-size     10
                       :color         :white
                       :font-weight   "100"
@@ -53,18 +114,7 @@
                       :text-align    "center"}}
         (str (count @entries) " entries")]
 
-       [touchable-highlight
-        {:style    {:background-color "blue"
-                    :padding-left     20
-                    :padding-right    20
-                    :padding-top      12
-                    :padding-bottom   12
-                    :margin-right     20}
-         :on-press #(swap! local update-in [:cam] not)}
-        [text {:style {:color       "white"
-                       :text-align  "center"
-                       :font-weight "bold"}}
-         (if (:cam @local) "hide cam" "show cam")]]
+       [menu-bar local]
 
        (when-let [barcode (:barcode @local)]
          [text {:style {:font-size     10
@@ -74,93 +124,49 @@
                         :text-align    "center"}}
           (str barcode)])
 
-       (if (:cam @local)
-         [cam {:style {:width  300
-                       :height 300}
-               :onBarCodeRead on-barcode-read}
-          [text {:style {:font-size     20
-                         :color         :white
-                         :font-weight   "300"
-                         :margin-bottom 5
-                         :text-align    "center"}}
-           "take"]]
-         [text-input {:style          {:height           200
-                                       :font-weight      "100"
-                                       :padding          10
-                                       :font-size        20
-                                       :background-color "#CCC"
-                                       :width            "100%"}
-                      :multiline      true
-                      :default-value  (:md @local)
-                      :keyboard-type  "twitter"
-                      :on-change-text (fn [text]
-                                        (swap! local assoc-in [:md] text))}])
-       [view {:style {:flex-direction "row"
-                      :padding-top    10
-                      :padding-bottom 10
-                      :padding-left   20
-                      :padding-right  20}}
-        [touchable-highlight
-         {:style    {:background-color "green"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12
-                     :margin-right     20}
-          :on-press #(let [put-fn @put-fn-atom
-                           new-entry (p/parse-entry (:md @local))
-                           new-entry-fn (h/new-entry-fn put-fn new-entry nil)]
-                       (new-entry-fn)
-                       (swap! local assoc-in [:md] ""))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "new"]]
-        [touchable-highlight
-         {:style    {:background-color "blue"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12
-                     :margin-right     20}
-          :on-press #(let [put-fn @put-fn-atom]
-                       (dotimes [n 75]
-                         (put-fn [:healthkit/steps n])))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "steps"]]
-        [touchable-highlight
-         {:style    {:background-color "blue"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12
-                     :margin-right     20}
-          :on-press #(let [put-fn @put-fn-atom]
-                       (put-fn [:healthkit/weight]))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "weight"]]
-        [touchable-highlight
-         {:style    {:background-color "#999"
-                     :padding-left     20
-                     :padding-right    20
-                     :padding-top      12
-                     :padding-bottom   12}
-          :on-press #(let [put-fn @put-fn-atom]
-                       (put-fn [:healthkit/bp]))}
-         [text {:style {:color       "white"
-                        :text-align  "center"
-                        :font-weight "bold"}}
-          "bp"]]]
-       [text {:style {:font-size     10
-                      :font-weight   "500"
-                      :color         "#CCC"
-                      :margin-bottom 20
-                      :text-align    "center"}}
-        (with-out-str (pp/pprint (second (last (sort-by first @entries)))))]
+
+       [tab-bar {:style {:tintColor        :black
+                         :height           10
+                         :background-color "#BBB"}}
+        [tab-bar-item {:title "Steps"}
+         [text {:style {:font-size     10
+                        :color         :white
+                        :font-weight   "100"
+                        :margin-bottom 5
+                        :text-align    "center"}}
+          "Steps"]]
+        [tab-bar-item {:title "BP"}
+         [text {:style {:font-size     10
+                        :color         :white
+                        :font-weight   "100"
+                        :margin-bottom 5
+                        :text-align    "center"}}
+          "BP"]]]
+
+       [view {:style {:flex  2
+                      :width "100%"}}
+        (if (:cam @local)
+          [cam {:style         {:width  "100%"
+                                :height 300}
+                :onBarCodeRead on-barcode-read}]
+          [text-input {:style          {:flex             1
+                                        :font-weight      "100"
+                                        :padding          10
+                                        :font-size        20
+                                        :background-color "#CCC"
+                                        :width            "100%"}
+                       :multiline      true
+                       :default-value  (:md @local)
+                       :keyboard-type  "twitter"
+                       :on-change-text (fn [text]
+                                         (swap! local assoc-in [:md] text))}])]
+
+       #_[text {:style {:font-size     10
+                        :font-weight   "500"
+                        :color         "#CCC"
+                        :margin-bottom 20
+                        :text-align    "center"}}
+          (with-out-str (pp/pprint (second (last (sort-by first @entries)))))]
        #_[image {:source logo-img
                  :style  {:width         80
                           :height        80
