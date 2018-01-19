281cf88325453e73c445359a17569c5d70c530d3
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-01-19 23:03:33 +0100

    delete old app version

--- mobile/src/cljs/i_was_where_app/subs.cljs
+++
@@ -1,5 +0,0 @@
-(ns i-was-where-app.subs
-  (:require [re-frame.core :refer [reg-sub]]))
-
-(reg-sub :stats (fn [db _] (:stats db)))
-(reg-sub :entries (fn [db _] (:entries db)))
--- mobile/test/i_was_where_app/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns i-was-where-app.core-test
-  (:require [clojure.test :refer :all]
-            [i-was-where-app.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
