f582936a08a7e17d94ed00b2a3ccda1333b0169b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-02-04 21:40:28 +0100

    vclock-comparator

--- src/cljc/meo/common/utils/vclock.cljc
+++ src/cljc/meo/common/utils/vclock.cljc
@@ -16,3 +16,25 @@
 (defn set-latest-vclock [entry node-id new-global-vclock]
   (let [latest-vclock-cnt (get-in new-global-vclock [node-id])]
     (assoc-in entry [:vclock node-id] latest-vclock-cnt)))
+
+(defn vlock-comparator [a b]
+  "Compares two vector clock maps. Those maps consist of node id strings as keys
+   and an integer, which is the counter on the particular node associated with
+   persisting the particular entry. See examples in the tests.
+   Will return :a>b if vclock a is strictly greater than b, :b>a in the opposite
+   case, :equal if they are the same, and :conflict if there is are conflict
+   that requires user interaction."
+  (let [cmps (set (map (fn [[k av]]
+                         (let [bv (or (get b k 0))]
+                           (cond
+                             (> av bv) :a>b
+                             (> bv av) :b>a
+                             :else :equal)))
+                       a))]
+    (cond
+      (= a b) :equal
+      (and (contains? cmps :a>b)
+           (not (contains? cmps :b>a))) :a>b
+      (and (contains? cmps :b>a)
+           (not (contains? cmps :a>b))) :b>a
+      :else :conflict)))
--- test/meo/vclock_test.clj
+++ test/meo/vclock_test.clj
@@ -2,53 +2,86 @@
   (:require [clojure.test :refer :all]
             [meo.common.utils.vclock :as vc]))
 
+(def some-node-id "some-node-id")
+(def another-node-id "another-node-id")
+
 (deftest new-global-vclock-test
   (testing "returns latest vclock values"
-    (is (= {"some-node-id"    2
-            "another-node-id" 1}
+    (is (= {some-node-id    2
+            another-node-id 1}
            (vc/new-global-vclock
-             {"some-node-id"    1
-              "another-node-id" 1}
-             {:vclock {"some-node-id" 2}}))))
+             {some-node-id    1
+              another-node-id 1}
+             {:vclock {some-node-id 2}}))))
 
   (testing "returns original vclock when entry already known"
-    (is (= {"some-node-id"    2
-            "another-node-id" 1}
+    (is (= {some-node-id    2
+            another-node-id 1}
            (vc/new-global-vclock
-             {"some-node-id"    2
-              "another-node-id" 1}
-             {"some-node-id" 2}))))
+             {some-node-id    2
+              another-node-id 1}
+             {some-node-id 2}))))
 
   (testing "ignores non-numbers"
-    (is (= {"some-node-id"    2
-            "another-node-id" 1}
+    (is (= {some-node-id    2
+            another-node-id 1}
            (vc/new-global-vclock
-             {"some-node-id"    2
-              "another-node-id" 1}
-             {"some-node-id" :foo})))))
+             {some-node-id    2
+              another-node-id 1}
+             {some-node-id :foo})))))
 
 (deftest next-global-vclock-test
   (testing "next global vclock state is generated correctly"
-    (is (= {"another-node-id" 1
-            "some-node-id"    3}
+    (is (= {another-node-id 1
+            some-node-id    3}
            (vc/next-global-vclock
-             {:cfg           {:node-id "some-node-id"}
-              :global-vclock {"some-node-id"    2
-                              "another-node-id" 1}}))))
+             {:cfg           {:node-id some-node-id}
+              :global-vclock {some-node-id    2
+                              another-node-id 1}}))))
 
   (testing "add node id when not known yet"
-    (is (= {"another-node-id"     1
-            "some-node-id"        2
+    (is (= {another-node-id       1
+            some-node-id          2
             "yet-another-node-id" 1}
            (vc/next-global-vclock
              {:cfg           {:node-id "yet-another-node-id"}
-              :global-vclock {"some-node-id"    2
-                              "another-node-id" 1}})))))
+              :global-vclock {some-node-id    2
+                              another-node-id 1}})))))
 
 (deftest set-latest-vclock-test
   (testing "vclock set correctly"
-    (is (= {:vclock {"some-node-id" 2}}
+    (is (= {:vclock {some-node-id 2}}
            (vc/set-latest-vclock
-             {:vclock {"some-node-id" 2}}
-             "some-node-id"
-             {"some-node-id" 2})))))
+             {:vclock {some-node-id 2}}
+             some-node-id
+             {some-node-id 2})))))
+
+(deftest vlock-comparator-test
+  (testing "return :equal when both clocks are the same"
+    (is (= :equal
+           (vc/vlock-comparator
+             {some-node-id    1
+              another-node-id 2}
+             {some-node-id    1
+              another-node-id 2}))))
+  (testing "returns :conflict when there is a conflict"
+    (is (= :conflict
+           (vc/vlock-comparator
+             {some-node-id    2
+              another-node-id 1}
+             {some-node-id    1
+              another-node-id 2}))))
+  (testing "returns :a>b when a is strictly greater than b"
+    (is (= :a>b
+           (vc/vlock-comparator
+             {some-node-id    2
+              another-node-id 1}
+             {some-node-id    1
+              another-node-id 1}))))
+  (testing "returns :b>a when b is strictly greater than a"
+    (is (= :a>b
+           (vc/vlock-comparator
+             {some-node-id    2
+              another-node-id 1}
+             {some-node-id    1
+              another-node-id 1})))))
