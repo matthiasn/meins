89baa854f3f7d65f0a94dc14effdadd8a40803f0
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-03-24 14:44:06 +0100

    assign stars to photos

--- src/cljs/meo/electron/renderer/ui/entry/thumbnails.cljs
+++ src/cljs/meo/electron/renderer/ui/entry/thumbnails.cljs
@@ -3,17 +3,36 @@
             [re-frame.core :refer [subscribe]]
             [react-responsive-carousel :as rrc]
             [reagent.ratom :refer-macros [reaction]]
+            [taoensso.timbre :refer [info error]]
             [meo.common.utils.misc :as u]
             [clojure.string :as s]
             [electron :refer [remote]]
             [cljs.pprint :as pp]
             [meo.common.utils.parse :as up]
             [meo.electron.renderer.helpers :as h]
-            [reagent.core :as r]))
+            [reagent.core :as r]
+            [meo.electron.renderer.ui.entry.utils :as eu]))
 
 (def iww-host (.-iwwHOST js/window))
 (def user-data (.getPath (aget remote "app") "userData"))
 (def photos (str user-data "/data/images/"))
+
+(defn stars-view [ts put-fn]
+  (let [{:keys [entry]} (eu/entry-reaction ts)
+        star (fn [idx n]
+               (let [click (fn [ev]
+                             (let [updated (assoc-in @entry [:stars] idx)]
+                               (put-fn [:entry/update updated])))]
+                 [:i.fa-star {:class    (if (<= idx n) "fas" "fal")
+                              :on-click click}]))]
+    (fn [ts put-fn]
+      (let [stars (:stars @entry 0)]
+        [:span.stars
+         [star 1 stars]
+         [star 2 stars]
+         [star 3 stars]
+         [star 4 stars]
+         [star 5 stars]]))))
 
 (defn image-view
   "Renders image view. Used resized and properly rotated image endpoint
@@ -24,18 +43,19 @@
           resized (if (s/includes? (s/lower-case path) ".jpg")
                     (str "http://" iww-host "/photos2/" file query-params)
                     path)
-          tab-group (:tab-group local-cfg)
           ts (:timestamp entry)
-          add-search (up/add-search (str (:timestamp entry)) tab-group put-fn)
+          external (str photos file)
           fullscreen (fn [ev] (swap! local update-in [:fullscreen] not))]
       [:div
        [:img {:src resized}]
-       [:p.legend {:on-click fullscreen}
-        [:a {:href path :target "_blank"}
-         (h/localize-datetime-full ts locale)
+       [:p.legend
+        [stars-view ts put-fn]
+        (h/localize-datetime-full ts locale)
+        [:span {:on-click fullscreen}
          (if (:fullscreen @local)
            [:i.fas.fa-compress]
-           [:i.fas.fa-expand])]]])))
+           [:i.fas.fa-expand])]
+        [:a {:href external :target "_blank"} [:i.fas.fa-external-link-alt]]]])))
 
 (defn carousel [_]
   (let [locale (subscribe [:locale])]
@@ -58,31 +78,43 @@
   (let [entries-map (subscribe [:entries-map])
         cfg (subscribe [:cfg])
         options (subscribe [:options])
-        active (reaction (:active @cfg))
         show-pvt? (reaction (:show-pvt @cfg))
         local (r/atom {:selected 0})
         get-or-retrieve (u/find-missing-entry entries-map put-fn)
+        linked-entries-set (reaction (set (:linked-entries-list entry)))
+        with-imgs (reaction (filter :img-file
+                                    (map get-or-retrieve @linked-entries-set)))
+        filtered (reaction (if @show-pvt?
+                             @with-imgs
+                             (filter (u/pvt-filter @options @entries-map)
+                                     @with-imgs)))
         escape (fn [ev]
                  (let [key-code (.. ev -keyCode)
-                       meta-key (.-metaKey ev)]
+                       meta-key (.-metaKey ev)
+                       set-stars (fn [n]
+                                   (info :count (count @filtered))
+                                   (let [selected (:selected @local)
+                                         current (get (vec @filtered) selected)
+                                         updated (assoc-in current [:stars] n)]
+                                     (put-fn [:entry/update updated])))]
+                   (info key-code meta-key)
                    (when (= key-code 27)
                      (swap! local assoc-in [:fullscreen] false))
                    (when (and meta-key (= key-code 70))
                      (swap! local update-in [:fullscreen] not))
-                   (.preventDefault ev)))]
+                   (when (and meta-key (= key-code 49)) (set-stars 1))
+                   (when (and meta-key (= key-code 50)) (set-stars 2))
+                   (when (and meta-key (= key-code 51)) (set-stars 3))
+                   (when (and meta-key (= key-code 52)) (set-stars 4))
+                   (when (and meta-key (= key-code 53)) (set-stars 5))
+                   ;(.preventDefault ev)
+                   ))]
     (fn thumbnail-render [entry local-cfg put-fn]
-      (let [ts (:timestamp entry)
-            entry-active? (contains? (set (vals @active)) (:timestamp entry))
-            linked-entries-set (set (:linked-entries-list entry))
-            with-imgs (filter :img-file (map get-or-retrieve linked-entries-set))
-            filtered (if @show-pvt?
-                       with-imgs
-                       (filter (u/pvt-filter @options @entries-map) with-imgs))]
-        (when-not entry-active?
-          [:div.thumbnails {:class       (when (:fullscreen @local) "fullscreen")
-                            :on-key-down escape}
-           [carousel {:ts        ts
-                      :filtered  filtered
-                      :local-cfg local-cfg
-                      :local     local
-                      :put-fn    put-fn}]])))))
+      (let [ts (:timestamp entry)]
+        [:div.thumbnails {:class       (when (:fullscreen @local) "fullscreen")
+                          :on-key-down escape}
+         [carousel {:ts        ts
+                    :filtered  @filtered
+                    :local-cfg local-cfg
+                    :local     local
+                    :put-fn    put-fn}]]))))
