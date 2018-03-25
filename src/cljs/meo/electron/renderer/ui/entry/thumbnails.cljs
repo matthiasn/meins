37b7dc09b21c8aa4ca863a17b9aa5572332d3a4d
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-03-25 22:32:51 +0200

    better keyword event handling

--- src/cljs/meo/electron/renderer/ui/entry/thumbnails.cljs
+++ src/cljs/meo/electron/renderer/ui/entry/thumbnails.cljs
@@ -34,11 +34,11 @@
 (defn image-view
   "Renders image view. Used resized and properly rotated image endpoint
    when JPEG file requested."
-  [entry query-params local-cfg locale local put-fn]
+  [entry local-cfg locale local put-fn]
   (when-let [file (:img-file entry)]
     (let [path (str "http://" iww-host "/photos/" file)
           resized (if (s/includes? (s/lower-case path) ".jpg")
-                    (str "http://" iww-host "/photos2/" file query-params)
+                    (str "http://" iww-host "/photos2/" file)
                     path)
           ts (:timestamp entry)
           external (str photos file)
@@ -57,18 +57,17 @@
 (defn carousel [_]
   (let [locale (subscribe [:locale])]
     (fn [{:keys [ts filtered local-cfg local put-fn]}]
-      (when (seq filtered)
-        (into
-          [:> rrc/Carousel {:showThumbs        false
-                            :showStatus        (> (count filtered) 1)
-                            :useKeyboardArrows true
-                            :selectedItem      (:selected @local)
-                            :onChange          #(swap! local assoc-in [:selected] %)
-                            :transitionTime    250}]
-          (mapv (fn [entry] (image-view
-                              entry "?width=600" local-cfg @locale local put-fn))
-                filtered))))))
-
+      (let [fullscreen (:fullscreen @local)]
+        (when (seq filtered)
+          (into
+            [:> rrc/Carousel {:showThumbs        fullscreen
+                              :showStatus        (> (count filtered) 1)
+                              :useKeyboardArrows fullscreen
+                              :selectedItem      (:selected @local)
+                              :onChange          #(swap! local assoc-in [:selected] %)
+                              :transitionTime    0}]
+            (mapv (fn [entry] (image-view entry local-cfg @locale local put-fn))
+                  (sort-by :timestamp filtered))))))))
 (defn thumbnails
   "Renders thumbnails of photos in linked entries. Respects private entries."
   [entry local-cfg put-fn]
@@ -81,35 +80,39 @@
         linked-entries-set (reaction (set (:linked-entries-list entry)))
         with-imgs (reaction (filter :img-file
                                     (map get-or-retrieve @linked-entries-set)))
-        filtered (reaction (if @show-pvt?
-                             @with-imgs
-                             (filter (u/pvt-filter @options @entries-map)
-                                     @with-imgs)))
-        escape (fn [ev]
-                 (let [key-code (.. ev -keyCode)
-                       meta-key (.-metaKey ev)
-                       set-stars (fn [n]
-                                   (info :count (count @filtered))
-                                   (let [selected (:selected @local)
-                                         current (get (vec @filtered) selected)
-                                         updated (assoc-in current [:stars] n)]
-                                     (put-fn [:entry/update updated])))]
-                   (info key-code meta-key)
-                   (when (= key-code 27)
-                     (swap! local assoc-in [:fullscreen] false))
-                   (when (and meta-key (= key-code 70))
-                     (swap! local update-in [:fullscreen] not))
-                   (when (and meta-key (= key-code 49)) (set-stars 1))
-                   (when (and meta-key (= key-code 50)) (set-stars 2))
-                   (when (and meta-key (= key-code 51)) (set-stars 3))
-                   (when (and meta-key (= key-code 52)) (set-stars 4))
-                   (when (and meta-key (= key-code 53)) (set-stars 5))
-                   ;(.preventDefault ev)
-                   ))]
+        filtered (reaction
+                   (if @show-pvt?
+                     @with-imgs
+                     (filter (u/pvt-filter @options @entries-map) @with-imgs)))
+        keydown (fn [ev]
+                  (let [key-code (.. ev -keyCode)
+                        meta-key (.-metaKey ev)
+                        set-stars (fn [n]
+                                    (info :count (count @filtered))
+                                    (let [selected (:selected @local)
+                                          current (get (vec @filtered) selected)
+                                          updated (assoc-in current [:stars] n)]
+                                      (put-fn [:entry/update updated])))]
+                    (info key-code meta-key)
+                    (when (= key-code 27)
+                      (swap! local assoc-in [:fullscreen] false))
+                    (when (and meta-key (= key-code 70))
+                      (swap! local update-in [:fullscreen] not))
+                    (when (and meta-key (= key-code 49)) (set-stars 1))
+                    (when (and meta-key (= key-code 50)) (set-stars 2))
+                    (when (and meta-key (= key-code 51)) (set-stars 3))
+                    (when (and meta-key (= key-code 52)) (set-stars 4))
+                    (when (and meta-key (= key-code 53)) (set-stars 5))
+                    (.stopPropagation ev)))
+        start-watch #(.addEventListener js/document "keydown" keydown)
+        stop-watch #(.removeEventListener js/document "keydown" keydown)]
     (fn thumbnail-render [entry local-cfg put-fn]
       (let [ts (:timestamp entry)]
-        [:div.thumbnails {:class       (when (:fullscreen @local) "fullscreen")
-                          :on-key-down escape}
+        [:div.thumbnails {:class          (when (:fullscreen @local) "fullscreen")
+                          :on-mouse-enter start-watch
+                          :on-mouse-over  start-watch
+                          :on-mouse-leave stop-watch}
+         ^{:key (str ts (:fullscreen @local))}
          [carousel {:ts        ts
                     :filtered  @filtered
                     :local-cfg local-cfg
