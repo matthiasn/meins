0dfb5c30b32e9a7aea7a27491f6ac700a3707dda
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-03-29 02:41:15 +0200

    ClojureScript-only implementation of carousel

--- src/clj/meo/jvm/routes/images.clj
+++ src/clj/meo/jvm/routes/images.clj
@@ -8,23 +8,32 @@
             [image-resizer.scale-methods :refer :all]
             [image-resizer.rotate :refer :all]
             [image-resizer.util :refer :all]
+            [ring.middleware.params :as params]
             [meo.jvm.imports.media :as im]
             [clojure.string :as s]
+            [taoensso.timbre :refer [info error warn]]
             [meo.jvm.file-utils :as fu]))
 
 (def img-resized-route
-  (GET "/photos2/:filename" [filename :as r]
-    (let [filename (str fu/img-path filename)
-          file (io/file filename)
-          exif (im/extract-exif file)
-          orientation (get exif "Orientation" "")
-          rotate-fn (cond (s/includes? orientation "(Rotate 90 CW)")
-                          (rotate-270-counter-clockwise-fn)
-                          (s/includes? orientation "(Rotate 180)")
-                          (rotate-180-counter-clockwise-fn)
-                          (s/includes? orientation "(Rotate 270 CW)")
-                          (rotate-90-counter-clockwise-fn)
-                          :else buffered-image)]
-      (-> (buffered-image file)
-          (rotate-fn)
-          (as-stream-by-mime-type "image/jpeg")))))
+  (params/wrap-params
+    (GET "/photos2/:filename" [filename :as r]
+      (let [filename (str fu/img-path filename)
+            file (io/file filename)
+            exif (im/extract-exif file)
+            orientation (get exif "Orientation" "")
+            rotate (cond (s/includes? orientation "(Rotate 90 CW)")
+                         (rotate-270-counter-clockwise-fn)
+                         (s/includes? orientation "(Rotate 180)")
+                         (rotate-180-counter-clockwise-fn)
+                         (s/includes? orientation "(Rotate 270 CW)")
+                         (rotate-90-counter-clockwise-fn)
+                         :else buffered-image)
+            params (:params r)
+            width (Integer/parseInt (or (get params "width") "1024"))
+            height (Integer/parseInt (or (get params "height") "1024"))
+            resize (resize-fn width height)]
+        (debug "request" r "width" width "height" height)
+        (-> (buffered-image file)
+            (rotate)
+            (resize)
+            (as-stream-by-mime-type "image/jpeg"))))))
--- src/cljs/meo/electron/renderer/ui/entry/thumbnails.cljs
+++ src/cljs/meo/electron/renderer/ui/entry/thumbnails.cljs
@@ -1,8 +1,8 @@
 (ns meo.electron.renderer.ui.entry.thumbnails
   (:require [re-frame.core :refer [subscribe]]
-            [react-responsive-carousel :as rrc]
             [reagent.ratom :refer-macros [reaction]]
             [taoensso.timbre :refer [info error debug]]
+            [clojure.data.avl :as avl]
             [meo.common.utils.misc :as u]
             [clojure.string :as s]
             [electron :refer [remote]]
@@ -21,32 +21,29 @@
         star (fn [idx n]
                (let [click (fn [ev]
                              (let [updated (assoc-in @entry [:stars] idx)]
+                               (debug "stars click" updated)
                                (put-fn [:entry/update updated])))]
                  [:i.fa-star {:class    (if (<= idx n) "fas" "fal")
-                              :on-click click}]))]
-    (fn [ts put-fn]
-      (let [stars (:stars @entry 0)]
-        [:span.stars
-         [star 1 stars]
-         [star 2 stars]
-         [star 3 stars]
-         [star 4 stars]
-         [star 5 stars]]))))
+                              :on-click click}]))
+        stars (:stars @entry 0)]
+    [:span.stars
+     [star 1 stars]
+     [star 2 stars]
+     [star 3 stars]
+     [star 4 stars]
+     [star 5 stars]]))
 
 (defn image-view
   "Renders image view. Uses resized and properly rotated image endpoint
    when JPEG file requested."
   [entry local-cfg locale local put-fn]
   (when-let [file (:img-file entry)]
-    (let [path (str "http://" iww-host "/photos/" file)
-          resized (if (s/includes? (s/lower-case path) ".jpg")
-                    (str "http://" iww-host "/photos2/" file)
-                    path)
+    (let [resized (str "http://" iww-host "/photos2/" file "?width=2048")
           ts (:timestamp entry)
           external (str photos file)
           html (md/md->html (:md entry))
           fullscreen (fn [ev] (swap! local update-in [:fullscreen] not))]
-      [:div
+      [:div.slide
        [:img {:src resized}]
        [:div.legend
         (h/localize-datetime-full ts locale)
@@ -58,25 +55,37 @@
         [:a {:href external :target "_blank"} [:i.fas.fa-external-link-alt]]
         [:div {:dangerouslySetInnerHTML {:__html html}}]]])))
 
+(defn thumb-view [entry local]
+  (when-let [file (:img-file entry)]
+    (let [resized (str "http://" iww-host "/photos2/" file "?width=256")
+          click (fn [_] (swap! local assoc-in [:selected] entry))]
+      [:li.thumb
+       {:on-click click
+        :class    (when (= entry (:selected @local)) "selected")}
+       [:img {:src resized}]])))
+
 (defn carousel [_]
-  (let [locale (subscribe [:locale])
-        carousel (r/adapt-react-class rrc/Carousel)]
-    (fn [{:keys [ts filtered local-cfg local put-fn]}]
+  (let [locale (subscribe [:locale])]
+    (fn [{:keys [ts filtered local-cfg local put-fn avl-sorted
+                 prev-click next-click]}]
       (let [fullscreen (:fullscreen @local)
-            locale @locale]
+            locale @locale
+            selected (or (:selected @local) (first filtered))]
         (when (seq filtered)
-          [carousel
-           {:showThumbs        fullscreen
-            :infiniteLoop      true
-            :showStatus        (> (count filtered) 1)
-            :useKeyboardArrows fullscreen
-            :showIndicators    false
-            ;:selectedItem      (:selected @local)
-            ;:onChange          #(swap! local assoc-in [:selected] %)
-            :transitionTime    0}
-           (for [entry filtered]
-             ^{:key ts}
-             [image-view entry local-cfg locale local put-fn])])))))
+          [:div
+           [:div.carousel.carousel-slider
+            {:style {:width "100%"}}
+            [:button.control-arrow.control-prev {:on-click prev-click}]
+            [:div.slider-wrapper.axis-horizontal
+             [image-view selected local-cfg locale local put-fn]]
+            [:button.control-arrow.control-next {:on-click next-click}]]
+           (when fullscreen
+             [:div.carousel
+              [:div.thumbs-wrapper.axis-horizontal
+               [:ul
+                (for [entry filtered]
+                  ^{:key (:timestamp entry)}
+                  [thumb-view entry local])]]])])))))
 
 (defn thumbnails
   "Renders thumbnails of photos in linked entries. Respects private entries."
@@ -85,7 +94,7 @@
         cfg (subscribe [:cfg])
         options (subscribe [:options])
         show-pvt? (reaction (:show-pvt @cfg))
-        local (r/atom {:selected 0})
+        local (r/atom {})
         get-or-retrieve (u/find-missing-entry entries-map put-fn)
         linked-comments-set (reaction
                               (set/union
@@ -97,21 +106,29 @@
                    (if @show-pvt?
                      @with-imgs
                      (filter (u/pvt-filter @options @entries-map) @with-imgs)))
+        cmp (fn [a b] (compare (:timestamp a) (:timestamp b)))
+        avl-sorted (reaction (into (avl/sorted-set-by cmp) (filter identity @filtered)))
         sorted (reaction (sort-by :timestamp @filtered))
+        selected (reaction (:selected @local))
+        next-click #(let [slide (avl/nearest @avl-sorted > @selected)]
+                      (swap! local assoc-in [:selected] slide))
+        prev-click #(let [slide (avl/nearest @avl-sorted < @selected)]
+                      (swap! local assoc-in [:selected] slide))
         keydown (fn [ev]
                   (let [key-code (.. ev -keyCode)
                         meta-key (.-metaKey ev)
                         set-stars (fn [n]
-                                    (info :count (count @filtered))
                                     (let [selected (:selected @local)
-                                          current (get (vec @sorted) selected)
-                                          updated (assoc-in current [:stars] n)]
+                                          updated (assoc-in selected [:stars] n)]
+                                      (debug updated)
                                       (put-fn [:entry/update updated])))]
                     (debug key-code meta-key)
                     (when (= key-code 27)
                       (swap! local assoc-in [:fullscreen] false))
                     (when (and meta-key (= key-code 70))
                       (swap! local update-in [:fullscreen] not))
+                    (when (= key-code 37) (prev-click))
+                    (when (= key-code 39) (next-click))
                     (when (and meta-key (= key-code 49)) (set-stars 1))
                     (when (and meta-key (= key-code 50)) (set-stars 2))
                     (when (and meta-key (= key-code 51)) (set-stars 3))
@@ -126,9 +143,11 @@
                           :on-mouse-enter start-watch
                           :on-mouse-over  start-watch
                           :on-mouse-leave stop-watch}
-         ^{:key (str ts (:fullscreen @local))}
-         [carousel {:ts        ts
-                    :filtered  @sorted
-                    :local-cfg local-cfg
-                    :local     local
-                    :put-fn    put-fn}]]))))
+         [carousel {:ts         ts
+                    :filtered   @sorted
+                    :avl-sorted avl-sorted
+                    :local-cfg  local-cfg
+                    :local      local
+                    :next-click next-click
+                    :prev-click prev-click
+                    :put-fn     put-fn}]]))))
