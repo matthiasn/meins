6cd0bdcbab8693e080b6cee3f5eac576f919f136
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-04-06 02:58:42 +0200

    search in map bounds

--- src/clj/meo/jvm/graph/geo.clj
+++ src/clj/meo/jvm/graph/geo.clj
@@ -1,28 +1,25 @@
 (ns meo.jvm.graph.geo
   (:require [meo.jvm.graph.query :as gq]
             [taoensso.timbre :refer [info error warn debug]]
-            [geo [geohash :as geohash]
-             [jts :as jts] [spatial :as sp] [io :as gio]]
-            ))
+            [geo [spatial :as sp]])
+  (:import [org.locationtech.spatial4j.shape.impl RectangleImpl]))
 
 (defn photos-within-bounds [{:keys [current-state msg-payload put-fn]}]
   (let [{:keys [ne sw center]} msg-payload
         n Integer/MAX_VALUE
         res (gq/get-filtered current-state {:tags #{"#photo"} :n n})
         entries (vals (:entries-map res))
-        center (sp/spatial4j-point (:lat center) (:lon center))
-        ne (sp/spatial4j-point (:lat ne) (:lon ne))
-        sw (sp/spatial4j-point (:lat sw) (:lon sw))
-        diagonal (sp/distance ne sw)
-        r (/ diagonal 2)
-        circle (sp/circle center r)
+        center (sp/point (:lat center) (:lon center))
+        ne (sp/point (:lat ne) (:lon ne))
+        sw (sp/point (:lat sw) (:lon sw))
+        rect (RectangleImpl. sw ne sp/earth)
         nearby (fn [entry]
                  (let [{:keys [latitude longitude img-file]} entry]
                    (when (and latitude longitude img-file)
-                     (let [point (sp/spatial4j-point latitude longitude)]
-                       (sp/intersects? point circle)))))
+                     (let [point (sp/point latitude longitude)]
+                       (sp/intersects? rect point)))))
         entries (filter nearby entries)
         timestamps (map :timestamp entries)]
-    (info "photos-within-bounds" (count entries) msg-payload)
+    (info "photos-within-bounds" (count entries) msg-payload (sp/area rect))
     {:emit-msg [:search/res {:type :geo-photos
                              :data timestamps}]}))
