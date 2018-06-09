593c46d332ae61a03866e998d63bfb9addc46444
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-06-09 03:27:54 +0200

    linux publish script

---
+++ publish_linux.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+yarn install
+
+mkdir bin
+rm -rf ./dist
+lein dist
+
+# replace symlinks, they lead to problems with electron-packager
+# from: https://superuser.com/questions/303559/replace-symbolic-links-with-files
+cd target
+tar -hcf - jlink | tar xf - -C ../bin/
+cd ..
+
+chmod -R +w bin/
+
+PLATFORMS=$1
+ELECTRON_BUILDER_COMPRESSION_LEVEL=3
+
+if [ "$2" == "release" ]; then
+  echo "Publishing Release"
+  electron-builder --publish always $1
+else
+  echo "Publishing Beta Version"
+  electron-builder -c electron-builder-beta.yml --publish always $1
+fi
