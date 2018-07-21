695e10b42910cdc9ee95c73e7b7578b06151bb34
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-07-21 18:35:32 +0200

    WIP: mobile deps

--- rn-app/project.clj
+++ rn-app/project.clj
@@ -4,23 +4,23 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
-                 [reagent "0.8.0-alpha2" :exclusions [cljsjs/react
-                                                      cljsjs/react-dom
-                                                      cljsjs/react-dom-server
-                                                      cljsjs/create-react-class]]
-                 [matthiasn/systems-toolbox "0.6.34"]
-                 [matthiasn/systems-toolbox-sente "0.6.27"]
-                 [cljs-react-navigation "0.1.1"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [reagent "0.8.1" :exclusions [cljsjs/react
+                                               cljsjs/react-dom
+                                               cljsjs/react-dom-server
+                                               cljsjs/create-react-class]]
+                 [matthiasn/systems-toolbox "0.6.37"]
+                 [matthiasn/systems-toolbox-sente "0.6.32"]
+                 [cljs-react-navigation "0.1.3"]
                  [org.clojure/data.avl "0.0.17"]
                  [core-async-storage "0.3.1"]
                  [re-frame "0.10.5"]]
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.15"]]
+            [lein-figwheel "0.5.16"]]
   :clean-targets ["target/" "index.ios.js" "index.android.js" #_($PLATFORM_CLEAN$)]
   :aliases {"prod-build" ["do" "clean" ["with-profile" "prod" "cljsbuild" "once" "ios"]]
             "prod-auto"  ["with-profile" "prod" "cljsbuild" "auto" "ios"]}
-  :profiles {:dev  {:dependencies [[figwheel-sidecar "0.5.15"]
+  :profiles {:dev  {:dependencies [[figwheel-sidecar "0.5.16"]
                                    [com.cemerick/piggieback "0.2.2"]]
                     :source-paths ["src" "env/dev"]
                     :cljsbuild    {:builds [{:id           "ios"
