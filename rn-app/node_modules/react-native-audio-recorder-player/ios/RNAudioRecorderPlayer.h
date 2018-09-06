04836bffc24fd69c47a403f24da34825a50a989e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-09-06 20:20:33 +0200

    save audio files in Documents/audio/

---
+++ rn-app/node_modules/react-native-audio-recorder-player/ios/RNAudioRecorderPlayer.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RNAudioRecorderPlayer : RCTEventEmitter <RCTBridgeModule, AVAudioPlayerDelegate>
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player
+        successfully:(BOOL)flag;
+- (void)updateRecorderProgress:(NSTimer*) timer;
+- (void)updateProgress:(NSTimer*) timer;
+- (void)startRecorderTimer;
+- (void)startPlayerTimer;
+@end
