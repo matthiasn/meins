8abf06959ea0eea8a7a963a012e33067d30d419f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2018-09-23 14:16:30 +0200

    fix habits

--- src/cljs/meo/electron/renderer/ui/menu.cljs
+++ src/cljs/meo/electron/renderer/ui/menu.cljs
@@ -37,6 +37,30 @@
   (let [spellcheck-handler (.-spellCheckHandler js/window)]
     (.switchLanguage spellcheck-handler cc)))
 
+(defn new-import-view [put-fn]
+  (let [local (r/atom {:show false})
+        open-new (fn [x]
+                   (put-fn [:search/add
+                            {:tab-group :left
+                             :query     (up/parse-search (:timestamp x))}]))]
+    (def ^:export new-entry (h/new-entry put-fn {} open-new))
+    (def ^:export new-story (h/new-entry put-fn {:entry_type :story} open-new))
+    (def ^:export new-saga (h/new-entry put-fn {:entry_type :saga} open-new))
+    (fn [put-fn]
+      (when (:show @local)
+        [:div.new-import
+         [:button.menu-new {:on-click (h/new-entry put-fn {} nil)}
+          [:span.fa.fa-plus-square] " new"]
+         [:button.menu-new
+          {:on-click (h/new-entry put-fn {:entry_type :saga} nil)}
+          [:span.fa.fa-plus-square] " new saga"]
+         [:button.menu-new
+          {:on-click (h/new-entry put-fn {:entry_type :story} nil)}
+          [:span.fa.fa-plus-square] " new story"]
+         [:button {:on-click #(do (put-fn [:import/photos])
+                                  (put-fn [:import/spotify]))}
+          [:span.fa.fa-map] " import"]]))))
+
 (defn upload-view []
   (let [cfg (subscribe [:cfg])
         iww-host (.-iwwHOST js/window)]
@@ -65,6 +89,7 @@
             today #(h/to-day (h/ymd (st/now)) pvt put-fn)]
         [:div.menu
          [:div.menu-header
+          [new-import-view put-fn]
           ;[:h1 {:on-click today} (h/localize-date day @locale)]
           [busy-status put-fn]
           [upload-view]]]))))
