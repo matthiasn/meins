d94291ae7e2bf4778326851428271d9d4069d594
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2019-03-15 01:33:37 +0100

    remove ts playground

--- meo-rn-ts/index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './out/src/App';
-AppRegistry.registerComponent('meoTs', () => App);
-
-import { YellowBox } from 'react-native'
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
