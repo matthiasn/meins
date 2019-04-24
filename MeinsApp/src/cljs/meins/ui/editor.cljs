a965c5c1333edd9796dc4d110e765991aec40e07
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2019-04-24 20:41:12 +0200

    clear editor on save

--- MeinsApp/src/cljs/meins/ui/editor.cljs
+++ MeinsApp/src/cljs/meins/ui/editor.cljs
@@ -11,9 +11,9 @@
 
 (def local (r/atom {:md ""}))
 
-(defn header [save-fn cancel-fn label]
+(defn header [_save-fn _cancel-fn _label]
   (let [theme (subscribe [:active-theme])]
-    (fn [save-fn cancel-fn]
+    (fn [save-fn cancel-fn label]
       (let [button-bg (get-in c/colors [:button-bg @theme])
             btn-text (get-in c/colors [:btn-text @theme])
             header-color (get-in c/colors [:header-text @theme])]
@@ -52,7 +52,7 @@
 (defn editor [_]
   (let [theme (subscribe [:active-theme])]
     (fn [{:keys [screenProps navigation] :as props}]
-      (let [{:keys [navigate goBack]} (js->clj navigation :keywordize-keys true)
+      (let [{:keys [navigate]} (js->clj navigation :keywordize-keys true)
             cancel-fn (fn []
                         (.dismiss keyboard)
                         (navigate "Journal"))
@@ -63,9 +63,7 @@
                        (navigate "Journal"))
             bg (get-in c/colors [:list-bg @theme])
             text-bg (get-in c/colors [:text-bg @theme])
-            text-color (get-in c/colors [:text @theme])
-            header-color (get-in c/colors [:header-text @theme])]
-        ;(swap! local2 assoc :navigate navigate)
+            text-color (get-in c/colors [:text @theme])]
         [view {:style {:display        "flex"
                        :flex-direction "column"
                        :padding-top    50}}
@@ -89,49 +87,8 @@
                                             :color            text-color
                                             :width            "100%"}
                        :multiline          true
-                       ;:default-value      (:md @local)
+                       :default-value      (:md @local)
                        :keyboard-type      "twitter"
                        :keyboardAppearance (if (= @theme :dark) "dark" "light")
                        :on-change-text     (fn [text]
                                              (swap! local assoc-in [:md] text))}]]]))))
-
-#_(defn editor-tab [local put-fn theme]
-    (let [local2 (r/atom {})
-          #_#_save-fn #(let [new-entry (p/parse-entry (:md @local))]
-                         (h/new-entry-fn put-fn new-entry)
-                         (swap! local assoc-in [:md] "")
-                         (when-let [navigate (:navigate @local2)]
-                           (.dismiss keyboard)
-                           (navigate "journal")))
-          cancel-fn #(when-let [navigate (:navigate @local2)]
-                       (.dismiss keyboard)
-                       (navigate "journal"))
-          header-bg (get-in c/colors [:header-tab @theme])
-          text-color (get-in c/colors [:text @theme])
-          header-right (fn [_]
-                         [touchable-opacity {;:on-press save-fn
-                                             :style {:padding-top    8
-                                                     :padding-left   12
-                                                     :padding-right  12
-                                                     :padding-bottom 8}}
-                          [text {:style {:color      "#0078e7"
-                                         :text-align "center"
-                                         :font-size  18}}
-                           "save"]])
-          header-left (fn [_]
-                        [touchable-opacity {:on-press cancel-fn
-                                            :style    {:padding-top    8
-                                                       :padding-left   12
-                                                       :padding-right  12
-                                                       :padding-bottom 8}}
-                         [text {:style {:color      "#0078e7"
-                                        :text-align "center"
-                                        :font-size  18}}
-                          "cancel"]])
-          opts {:title            "Add Entry"
-                :headerRight      header-right
-                :headerLeft       header-left
-                :headerTitleStyle {:color text-color}
-                :headerStyle      {:backgroundColor header-bg}}]
-      (stack-navigator
-        {:editor {:screen (stack-screen (editor local local2 put-fn) opts)}})))
