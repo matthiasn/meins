8e835f3984c5ea4f4684daf77db4e2609cce99c8
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2019-09-05 16:38:26 +0200

    Location aggregate entries

--- MeinsApp/src/cljs/meins/components/geolocation.cljs
+++ MeinsApp/src/cljs/meins/components/geolocation.cljs
@@ -13,7 +13,7 @@
     {:reset           true
      :desiredAccuracy (.-DESIRED_ACCURACY_HIGH BackgroundGeolocation)
      :distanceFilter  10
-     :stopTimeout     1
+     :stopTimeout     2
      :debug           false
      :logLevel        (.-LOG_LEVEL_VERBOSE BackgroundGeolocation)
      :stopOnTerminate false
@@ -21,28 +21,44 @@
      :batchSync       false
      :autoSync        false}))
 
-(defn state-fn [put-fn]
+(defn next-save-ts [ts]
+  (let [interval (* 5 60 1000)
+        n (js/Math.ceil (/ ts interval))]
+    (* interval n)))
+
+(defn empty-state []
+  {:next-save (next-save-ts (st/now))
+   :locations []})
+
+(defn stop [_]
+  (.stop BackgroundGeolocation)
+  {})
+
+(defn start [{:keys [cmp-state put-fn]}]
   (try
-    (let [state (atom {})
+    (let [save-entry (fn []
+                       (let [ts (st/now)
+                             dtf (new intl/DateTimeFormat)
+                             timezone (or (when-let [resolved (.-resolved dtf)]
+                                            (.-timeZone resolved))
+                                          (when-let [resolved (.resolvedOptions dtf)]
+                                            (.-timeZone resolved)))
+                             locations (:locations @cmp-state)
+                             entry {:md         (str (count locations) " locations recorded")
+                                    :timestamp  ts
+                                    :new-entry  true
+                                    :timezone   timezone
+                                    :utc-offset (.getTimezoneOffset (new js/Date))
+                                    :bg-geo     locations
+                                    :perm_tags  #{"#locationtracking"}}]
+                         (put-fn [:entry/new entry])))
           on-location (fn [loc]
                         (let [loc2 (->clj loc)
-                              coords (:coords loc2)
-                              ts (st/now)
-                              dtf (new intl/DateTimeFormat)
-                              timezone (or (when-let [resolved (.-resolved dtf)]
-                                             (.-timeZone resolved))
-                                           (when-let [resolved (.resolvedOptions dtf)]
-                                             (.-timeZone resolved)))
-                              entry {:md        (str loc2)
-                                     :timestamp  ts
-                                     :new-entry  true
-                                     :timezone   timezone
-                                     :utc-offset (.getTimezoneOffset (new js/Date))
-                                     :coords    coords
-                                     :perm_tags #{"#locationtracking"}
-                                     :latitude  (:latitude coords)
-                                     :longitude (:longitude coords)}]
-                          (put-fn [:entry/new entry])))
+                              now (st/now)]
+                          (swap! cmp-state update :locations conj loc2)
+                          (when (> now (:next-save @cmp-state))
+                            (save-entry)
+                            (reset! cmp-state (empty-state)))))
           on-motion (fn [m] (js/console.warn "motion" m))
           on-activity (fn [act] (js/console.warn "activity" act))
           on-error (fn [err] (js/console.error err))
@@ -53,11 +69,18 @@
       (.onLocation BackgroundGeolocation on-location on-error)
       ;(.onActivityChange BackgroundGeolocation on-activity)
       ;(.onMotionChange BackgroundGeolocation on-motion)
-      (.ready BackgroundGeolocation cfg on-ready)
-      {:state state})
-    (catch :default e (shared/alert (str "geolocation not available: " e)))))
+      (.ready BackgroundGeolocation cfg on-ready))
+    (catch :default e (shared/alert (str "geolocation not available: " e))))
+  {})
+
+(defn state-fn [put-fn]
+  (let [state (atom (empty-state))]
+    (start {:cmp-state state
+            :put-fn    put-fn})
+    {:state state}))
 
 (defn cmp-map [cmp-id]
   {:cmp-id      cmp-id
    :state-fn    state-fn
-   :handler-map {}})
+   :handler-map {:bg-geo/start start
+                 :bg-geo/stop  stop}})
