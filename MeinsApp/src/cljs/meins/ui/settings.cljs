3b44fbe0e8a8098b27ab5c6b95428e6625f34350
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2019-10-04 18:31:56 +0200

    new design in settings page

--- MeinsApp/src/cljs/meins/ui/settings.cljs
+++ MeinsApp/src/cljs/meins/ui/settings.cljs
@@ -7,102 +7,96 @@
             [meins.ui.settings.geolocation :as geo]
             [meins.ui.settings.health :as sh]
             [meins.ui.settings.sync :as sync]
-            [meins.ui.shared :refer [fa-icon settings-icon settings-list settings-list-header
-                                     settings-list-item status-bar view]]
+            [meins.ui.shared :refer [fa-icon settings-icon status-bar text touchable-opacity view]]
             [meins.ui.styles :as styles]
             [re-frame.core :refer [reg-sub subscribe]]
             [reagent.core :as r]))
 
-(def bg (get-in styles/colors [:list-bg :dark]))
+(def nav-bg (get-in styles/colors [:nav-bg :dark]))
 
 (def local (r/atom {:cam      false
                     :contacts (clj->js [])
                     :active   "journal"}))
+
+(defn item [_]
+  (let [theme (subscribe [:active-theme])]
+    (fn [{:keys [label info on-press]}]
+      (let [header-color (get-in styles/colors [:header-text @theme])]
+        [touchable-opacity {:on-press on-press
+                            :style    {:color             header-color
+                                       :margin-left       9
+                                       :margin-right      15
+                                       :margin-top        23
+                                       :display           :flex
+                                       :borderBottomColor "#BBBDBF"
+                                       :borderBottomWidth 0.5
+                                       :flex-direction    :row
+                                       :justify-content   :space-between}}
+         [text {:style {:font-size      12
+                        :line-height    22
+                        :padding-bottom 13
+                        :font-family    "Montserrat-Regular"
+                        :text-align     "center"
+                        :opacity        0.68
+                        :color          "white"}}
+          label]
+         (if info
+           [text {:style {:font-size   12
+                          :line-height 22
+                          :font-family "Montserrat-Medium"
+                          :text-align  "center"
+                          :color       "white"}}
+            info]
+           [fa-icon {:name  "angle-right"
+                     :size  30
+                     :style {:color       "#BBBDBF"
+                             :margin-left 25}}])]))))
 
 (defn settings-wrapper [_props]
   (let [theme (subscribe [:active-theme])]
     (fn [{:keys [navigation]}]
       (let [{:keys [navigate]} (js->clj navigation :keywordize-keys true)
             bg (get-in styles/colors [:list-bg @theme])
-            item-bg (get-in styles/colors [:button-bg @theme])
-            header-color (get-in styles/colors [:header-text @theme])
-            text-color (get-in styles/colors [:btn-text @theme])]
-        [view {:style {:flex-direction   "column"
+            nav-bg (get-in styles/colors [:nav-bg :dark])
+            header-color (get-in styles/colors [:header-text @theme])]
+        [view {:style {:display          "flex"
+                       :flex-direction   "column"
                        :height           "100%"
-                       :background-color bg}}
+                       :background-color nav-bg}}
          [status-bar {:barStyle "light-content"}]
-         [settings-list {:border-color bg
-                         :flex         1}
-          [settings-list-header
-           {:headerText       "Settings"
-            :background-color item-bg
-            :headerStyle      {:color       header-color
-                               :text-align  "center"
-                               :font-size   24}}]
-          [settings-list-item
-           {:hasNavArrow      false
-            :background-color item-bg
-            :title            "Version"
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "list" text-color)
-            :title-info       (aget rnvn "default" "appVersion")}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :title            "Contacts"
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "address-book" text-color)
-            :on-press         #(navigate "contacts")
-            :title-info       (str (.-length (:contacts @local)))}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :titleStyle       {:color text-color}
-            :title            "Health"
-            :icon             (settings-icon "heartbeat" text-color)
-            :on-press         #(navigate "health")}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :title            "Theme"
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "font" text-color)
-            :on-press         #(navigate "theme")}]
-          [settings-list-item
-           {:title            "Database"
-            :background-color item-bg
-            :hasNavArrow      true
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "database" text-color)
-            :on-press         #(navigate "db")}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "bug" text-color)
-            :on-press         #(navigate "dev")
-            :title            "Entry View"}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "map" text-color)
-            :on-press         #(navigate "geo")
-            :title            "Geolocation"}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "microphone" text-color)
-            :on-press         #(navigate "audio")
-            :title            "Audio"}]
-          [settings-list-item
-           {:hasNavArrow      true
-            :background-color item-bg
-            :titleStyle       {:color text-color}
-            :icon             (settings-icon "refresh" text-color)
-            :on-press         #(navigate "sync")
-            :title            "Sync"}]]]))))
+         [text {:style {:font-size      18
+                        :line-height    22
+                        :padding-bottom 20
+                        :letter-spacing 0.02
+                        :font-family    "Montserrat-SemiBold"
+                        :text-align     "center"
+                        :color          "white"}}
+          "Settings"]
+         [view {
+                :background-color bg
+                :style            {:color         header-color
+                                   :display       :flex
+                                   :padding-left  24
+                                   :padding-right 24
+                                   :height        "100%"}}
+          [item {:label "VERSION"
+                 :info  (aget rnvn "default" "appVersion")}]
+          [item {:label "CONTACTS"
+                 :info  0}]
+          [item {:label    "HEALTH"
+                 :on-press #(navigate "health")}]
+          [item {:label    "THEME"
+                 :on-press #(navigate "theme")}]
+          [item {:label    "DATABASE"
+                 :on-press #(navigate "db")}]
+          [item {:label    "ENTRY VIEW"
+                 :on-press #(navigate "dev")}]
+          [item {:label    "GEOLOCATION"
+                 :on-press #(navigate "geo")}]
+          [item {:label    "AUDIO"
+                 :on-press #(navigate "audio")}]
+          [item {:label    "SYNC"
+                 :on-press #(navigate "sync")}]]]))))
 
 (def settings-stack
   (createStackNavigator
@@ -113,5 +107,5 @@
               :db       {:screen (r/reactify-component db/db-settings)}
               :audio    {:screen (r/reactify-component audio/audio-settings)}
               :health   {:screen (r/reactify-component sh/health-settings)}})
-    (clj->js {:defaultNavigationOptions {:headerStyle {:backgroundColor   bg
+    (clj->js {:defaultNavigationOptions {:headerStyle {:backgroundColor   nav-bg
                                                        :borderBottomWidth 0}}})))
