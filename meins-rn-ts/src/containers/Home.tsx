fb9b15b0fd9e71f44ef1328a60827b519ab250f8
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-04-15 23:46:30 +0200

    refactor(meins2): home cleanup

--- meins-rn-ts/src/containers/Home.tsx
+++ meins-rn-ts/src/containers/Home.tsx
@@ -1,22 +1,11 @@
 import React, {memo} from 'react';
-import {useTranslation} from 'react-i18next';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Colors from 'src/constants/colors';
 import {AudioRecorder} from 'src/components/AudioRecorder';
 
 function Home() {
-  const {t} = useTranslation();
-
-  const instructions = Platform.select({
-    ios: t('iosInstruction'),
-    android: t('androidInstruction'),
-  });
-
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>{t('welcome')}</Text>
-      <Text style={styles.instructions}>{t('instructions')}</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
       <AudioRecorder />
     </View>
   );
@@ -30,16 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: Colors.aliceBlue,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: Colors.darkCharcoal,
-    marginBottom: 5,
+    backgroundColor: Colors.darkCharcoal,
   },
 });
