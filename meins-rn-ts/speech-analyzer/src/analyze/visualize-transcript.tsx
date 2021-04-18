35ff60a2367f7f47c294a7746ae3d4b1816b4410
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-04-19 01:26:54 +0200

    feat(meins2): show utterances log

--- meins-rn-ts/speech-analyzer/src/analyze/visualize-transcript.tsx
+++ meins-rn-ts/speech-analyzer/src/analyze/visualize-transcript.tsx
@@ -1,9 +1,27 @@
 import React, {useEffect, useState} from 'react'
 import {ASSEMBLY_API_KEY, DOC_ID} from './secrets'
 
+function pad(s: string, n: number) {
+  return s.padStart(2, '0')
+}
+
+function millisecondsToHuman(ms: number) {
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / 1000 / 60) % 60);
+  const hours = Math.floor((ms  / 1000 / 3600 ) % 24)
+
+  const humanized = [
+    pad(hours.toString(), 2),
+    pad(minutes.toString(), 2),
+    pad(seconds.toString(), 2),
+  ].join(':');
+
+  return humanized;
+}
+
 export function AnalyzeTranscript() {
   const [status, setStatus] = useState('idle')
-  const [data, setData] = useState()
+  const [data, setData] = useState<any>()
   console.log('ASSEMBLY_API_KEY', ASSEMBLY_API_KEY)
 
   useEffect(() => {
@@ -17,9 +35,8 @@ export function AnalyzeTranscript() {
           },
         }
       )
-      const json = await response.json()
-      console.log(json)
       const data = await response.json()
+      console.log(data)
       setData(data)
       setStatus('fetched')
     }
@@ -29,6 +46,18 @@ export function AnalyzeTranscript() {
   return (
     <div>
       <h1>Transcript Analytics</h1>
+      {data && data.utterances.map(({end, speaker, start, text}: {
+        end: number
+        speaker: string
+        start: number
+        text: string
+      }) => {
+        return (
+          <div className="utterance">
+            {speaker}({millisecondsToHuman(start)}): {text}
+          </div>
+        )
+      })}
     </div>
   )
 }
