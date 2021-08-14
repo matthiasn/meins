f7dd26a57a80a5f2d8ea541ed654bbf6c5de9d56
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-08-14 17:27:02 +0200

    feat: add basic scheme

--- wisely/lib/main.dart
+++ wisely/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisely/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: const MyHomePage(title: 'WISELY'),
     );
@@ -73,7 +74,11 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: AppColors.headerFontColor),
+        ),
+        backgroundColor: AppColors.headerBgColor,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
---
+++ wisely/lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static Color headerBgColor = Color.fromRGBO(68, 68, 85, 1);
+  static Color headerFontColor = Colors.white;
+}
+
+class AppNumbers {
+  static const double borderRadius = 3.0;
+}
