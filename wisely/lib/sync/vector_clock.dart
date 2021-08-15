9df51716d1c9f01ed483d9aaf9735eb19e09b356
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-08-15 16:28:55 +0200

    test: compare two vector clocks

--- wisely/lib/sync/vector_clock.dart
+++ wisely/lib/sync/vector_clock.dart
@@ -1,5 +1,4 @@
 import 'package:collection/collection.dart';
-import 'package:uuid/uuid.dart';
 
 enum VclockStatus {
   equal,
@@ -9,15 +8,53 @@ enum VclockStatus {
 }
 
 class VectorClock {
-  Map<Uuid, int> vclock = new Map();
+  Map<String, int> vclock = new Map<String, int>();
 
   VectorClock(this.vclock);
 
-  static VclockStatus compare(Map<Uuid, int> vc1, Map<Uuid, int> vc2) {
+  // Compares two vector clocks. A and B are maps with node id strings as keys
+  // and an integer as value, which is the offset on the node associated with
+  // persisting the particular entry. See examples in the tests.
+
+  // Will return VclockStatus.a_gt_b if clock A dominates B, VclockStatus.b_gt_a
+  // in the opposite case, VclockStatus.equal if they are the same, and
+  // VclockStatus.concurrent if no strict order could be determined.
+
+  // Throws an exception when input is invalid.
+  static VclockStatus compare(Map<String, int> vc1, Map<String, int> vc2) {
+    Set<VclockStatus> comparisons = new Set<VclockStatus>();
+    Set<String> nodeIds = new Set<String>();
+
     if (DeepCollectionEquality().equals(vc1, vc2)) {
       return VclockStatus.equal;
     }
 
-    throw ("failed to compare vector clocks");
+    nodeIds.addAll(vc1.keys);
+    nodeIds.addAll(vc2.keys);
+
+    for (String nodeId in nodeIds) {
+      int counterA = vc1[nodeId] ?? 0;
+      int counterB = vc2[nodeId] ?? 0;
+
+      if (counterA == counterB) {
+        comparisons.add(VclockStatus.equal);
+      } else if (counterA > counterB) {
+        comparisons.add(VclockStatus.a_gt_b);
+      } else if (counterB > counterA) {
+        comparisons.add(VclockStatus.b_gt_a);
+      }
+    }
+
+    if (comparisons.contains(VclockStatus.a_gt_b) &&
+        !comparisons.contains(VclockStatus.b_gt_a)) {
+      return VclockStatus.a_gt_b;
+    }
+
+    if (comparisons.contains(VclockStatus.b_gt_a) &&
+        !comparisons.contains(VclockStatus.a_gt_b)) {
+      return VclockStatus.b_gt_a;
+    }
+
+    return VclockStatus.concurrent;
   }
 }
--- wisely/test/vector_clock_test.dart
+++ wisely/test/vector_clock_test.dart
@@ -2,11 +2,82 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:uuid/uuid.dart';
 import 'package:wisely/sync/vector_clock.dart';
 
+String nodeId1 = Uuid().v1();
+String nodeId2 = Uuid().v1();
+String nodeId3 = Uuid().v1();
+
 void main() {
   test('Compare two empty clocks', () {
-    Map<Uuid, int> vc1 = new Map();
-    Map<Uuid, int> vc2 = new Map();
+    Map<String, int> vc1 = new Map();
+    Map<String, int> vc2 = new Map();
 
     expect(VectorClock.compare(vc1, vc2), VclockStatus.equal);
+  });
+
+  test('Compare two similar clocks', () {
+    Map<String, int> vc1 = new Map();
+    vc1.addEntries([
+      MapEntry(nodeId1, 1),
+      MapEntry(nodeId2, 1),
+      MapEntry(nodeId3, 1),
+    ]);
+    Map<String, int> vc2 = new Map();
+    vc2.addEntries([
+      MapEntry(nodeId3, 1),
+      MapEntry(nodeId2, 1),
+      MapEntry(nodeId1, 1),
+    ]);
+    expect(VectorClock.compare(vc1, vc2), VclockStatus.equal);
+  });
+
+  test('Compare two concurrent clocks', () {
+    Map<String, int> vc1 = new Map();
+    vc1.addEntries([
+      MapEntry(nodeId1, 1),
+      MapEntry(nodeId2, 2),
+      MapEntry(nodeId3, 4),
+    ]);
+
+    Map<String, int> vc2 = new Map();
+    vc2.addEntries([
+      MapEntry(nodeId1, 3),
+      MapEntry(nodeId2, 2),
+      MapEntry(nodeId3, 3),
+    ]);
+    expect(VectorClock.compare(vc1, vc2), VclockStatus.concurrent);
+  });
+
+  test('Compare two clocks where vc1 is greater than vc2', () {
+    Map<String, int> vc1 = new Map();
+    vc1.addEntries([
+      MapEntry(nodeId1, 3),
+      MapEntry(nodeId2, 3),
+      MapEntry(nodeId3, 3),
+    ]);
+
+    Map<String, int> vc2 = new Map();
+    vc2.addEntries([
+      MapEntry(nodeId1, 1),
+      MapEntry(nodeId2, 2),
+      MapEntry(nodeId3, 3),
+    ]);
+    expect(VectorClock.compare(vc1, vc2), VclockStatus.a_gt_b);
+  });
+
+  test('Compare two clocks where vc2 is greater than vc1', () {
+    Map<String, int> vc1 = new Map();
+    vc1.addEntries([
+      MapEntry(nodeId1, 1),
+      MapEntry(nodeId2, 2),
+      MapEntry(nodeId3, 3),
+    ]);
+
+    Map<String, int> vc2 = new Map();
+    vc2.addEntries([
+      MapEntry(nodeId1, 3),
+      MapEntry(nodeId2, 3),
+      MapEntry(nodeId3, 3),
+    ]);
+    expect(VectorClock.compare(vc1, vc2), VclockStatus.b_gt_a);
   });
 }
