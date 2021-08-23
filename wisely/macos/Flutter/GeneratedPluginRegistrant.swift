cec842d4903ea8c0cfd9b446b122513ce81b66d7
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-08-24 01:27:14 +0200

    feat: add health lib

--- wisely/macos/Flutter/GeneratedPluginRegistrant.swift
+++ wisely/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus_macos
 import location
 import path_provider_macos
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
