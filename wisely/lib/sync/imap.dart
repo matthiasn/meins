b294b3c65865da3db5ccb777d5a870695f15993a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-09-22 21:57:42 +0200

    workaround: disable imap_client lib because of incompatibility

--- wisely/lib/sync/imap.dart
+++ wisely/lib/sync/imap.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/services.dart';
-import 'package:imap_client/imap_client.dart';
+// import 'package:imap_client/imap_client.dart';
 import 'package:yaml/yaml.dart';
 
 class ImapSyncClient {
@@ -7,10 +7,10 @@ class ImapSyncClient {
   late String userName;
   late String password;
 
-  late ImapClient client;
+//  late ImapClient client;
 
   ImapSyncClient() {
-    client = new ImapClient();
+//    client = new ImapClient();
     init();
   }
 
@@ -22,17 +22,17 @@ class ImapSyncClient {
     userName = yamlMap['userName'];
     password = yamlMap['password'];
 
-    await client.connect(host, 993, true);
-    await client.login(userName, password);
-    client.capability().then((value) => print(value));
-    print(client);
-
-    ImapFolder inbox = await client.getFolder('Inbox');
-    print(inbox);
-
-    List<int> emails = await inbox.search('UNSEEN');
-    print(emails);
-    int emails1 = await inbox.mailCount;
-    print(emails1);
+    // await client.connect(host, 993, true);
+    // await client.login(userName, password);
+    // client.capability().then((value) => print(value));
+    // print(client);
+    //
+    // ImapFolder inbox = await client.getFolder('Inbox');
+    // print(inbox);
+    //
+    // List<int> emails = await inbox.search('UNSEEN');
+    // print(emails);
+    // int emails1 = await inbox.mailCount;
+    // print(emails1);
   }
 }
