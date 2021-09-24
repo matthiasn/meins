42dc86da6969226d6e4828f235a31ba73e747637
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-09-25 00:55:23 +0200

    test: compare vector clocks with more nodes

--- wisely/test/vector_clock_glados_test.dart
+++ wisely/test/vector_clock_glados_test.dart
@@ -8,6 +8,11 @@ extension AnyVectorClock on Any {
           (int v1, int v2) {
         return VectorClock({'a': v1, 'b': v2});
       });
+  Generator<VectorClock> get vc3 => any.combine3(
+          any.positiveIntOrZero, any.positiveIntOrZero, any.positiveIntOrZero,
+          (int v1, int v2, int v3) {
+        return VectorClock({'a': v1, 'b': v2, 'c': v3});
+      });
   Generator<VectorClock> get possiblyInvalidVc =>
       any.combine2(any.int, any.int, (int v1, int v2) {
         return VectorClock({'a': v1, 'b': v2});
@@ -28,6 +33,48 @@ void main() {
     } else if (vc1.get('a') <= vc2.get('a') &&
         vc1.get('b') <= vc2.get('b') &&
         (vc1.get('a') + vc1.get('b')) < (vc2.get('a') + vc2.get('b'))) {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.b_gt_a);
+    } else {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.concurrent);
+    }
+  });
+
+  Glados2<VectorClock, VectorClock>(any.vc3, any.vc3)
+      .test('compare two vector clocks with three nodes', (vc1, vc2) {
+    if (const DeepCollectionEquality().equals(vc1.vclock, vc2.vclock)) {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.equal);
+    } else if (vc1.get('a') >= vc2.get('a') &&
+        vc1.get('b') >= vc2.get('b') &&
+        vc1.get('c') >= vc2.get('c') &&
+        (vc1.get('a') + vc1.get('b') + vc1.get('c')) >
+            (vc2.get('a') + vc2.get('b') + vc2.get('c'))) {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.a_gt_b);
+    } else if (vc1.get('a') <= vc2.get('a') &&
+        vc1.get('b') <= vc2.get('b') &&
+        vc1.get('c') <= vc2.get('c') &&
+        (vc1.get('a') + vc1.get('b') + vc1.get('c')) <
+            (vc2.get('a') + vc2.get('b') + vc2.get('c'))) {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.b_gt_a);
+    } else {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.concurrent);
+    }
+  });
+
+  Glados2<VectorClock, VectorClock>(any.vc, any.vc3)
+      .test('compare two vector clocks, one with three nodes', (vc1, vc2) {
+    if (const DeepCollectionEquality().equals(vc1.vclock, vc2.vclock)) {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.equal);
+    } else if (vc1.get('a') >= vc2.get('a') &&
+        vc1.get('b') >= vc2.get('b') &&
+        vc1.get('c') >= vc2.get('c') &&
+        (vc1.get('a') + vc1.get('b') + vc1.get('c')) >
+            (vc2.get('a') + vc2.get('b') + vc2.get('c'))) {
+      expect(VectorClock.compare(vc1, vc2), VclockStatus.a_gt_b);
+    } else if (vc1.get('a') <= vc2.get('a') &&
+        vc1.get('b') <= vc2.get('b') &&
+        vc1.get('c') <= vc2.get('c') &&
+        (vc1.get('a') + vc1.get('b') + vc1.get('c')) <
+            (vc2.get('a') + vc2.get('b') + vc2.get('c'))) {
       expect(VectorClock.compare(vc1, vc2), VclockStatus.b_gt_a);
     } else {
       expect(VectorClock.compare(vc1, vc2), VclockStatus.concurrent);
