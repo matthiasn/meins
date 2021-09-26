b917dce81929982595ee46bfdd6479d8f5c28b6e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-09-26 19:52:02 +0200

    WIP: recorder and player

--- wisely/lib/pages/audio.dart
+++ wisely/lib/pages/audio.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter_sound/public/flutter_sound_player.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AudioPage extends StatefulWidget {
   const AudioPage({Key? key}) : super(key: key);
@@ -9,17 +13,73 @@ class AudioPage extends StatefulWidget {
 }
 
 class _AudioPageState extends State<AudioPage> {
+  FlutterSoundPlayer? _myPlayer = FlutterSoundPlayer();
+  FlutterSoundRecorder? _myRecorder = FlutterSoundRecorder();
+  bool _mPlayerIsInited = false;
+  bool _mRecorderIsInited = false;
+  String _mPath = '';
+
   @override
   void initState() {
     super.initState();
+    // Be careful : openAudioSession return a Future.
+    // Do not access your FlutterSoundPlayer or FlutterSoundRecorder before the completion of the Future
+    _myPlayer?.openAudioSession().then((value) {
+      setState(() {
+        _mPlayerIsInited = true;
+      });
+    });
+
+    _myRecorder?.openAudioSession().then((value) {
+      setState(() {
+        _mRecorderIsInited = true;
+      });
+    });
   }
 
-  void _record() async {
-    print('record');
+  @override
+  void dispose() {
+    // Be careful : you must `close` the audio session when you have finished with it.
+    _myPlayer?.closeAudioSession();
+    _myPlayer = null;
+    _myRecorder?.closeAudioSession();
+    _myRecorder = null;
+    super.dispose();
+  }
+
+  Future<void> _record() async {
+    var tempDir = await getTemporaryDirectory();
+    String _path = '${tempDir.path}/flutter_sound.aac';
+    print('RECORD: ${_path}');
+
+    await _myRecorder?.startRecorder(
+      toFile: _path,
+      codec: Codec.aacADTS,
+    );
+  }
+
+  Future<void> stopRecorder() async {
+    await _myRecorder?.stopRecorder();
   }
 
   void _play() async {
-    print('play');
+    var tempDir = await getTemporaryDirectory();
+    String _path = '${tempDir.path}/flutter_sound.aac';
+    print('PLAY: ${_path}');
+
+    await _myPlayer?.startPlayer(
+        fromURI: _mPath,
+        codec: Codec.mp3,
+        whenFinished: () {
+          setState(() {});
+        });
+    setState(() {});
+  }
+
+  Future<void> _stopPlayer() async {
+    if (_myPlayer != null) {
+      await _myPlayer?.stopPlayer();
+    }
   }
 
   void _pause() async {
@@ -27,6 +87,8 @@ class _AudioPageState extends State<AudioPage> {
   }
 
   void _stop() async {
+    stopRecorder();
+    _stopPlayer();
     print('stop');
   }
 
