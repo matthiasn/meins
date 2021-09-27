955944897f8bd894c5e6f200e22c85da32c16cf1
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-09-27 14:19:55 +0200

    feat: add audioplayers lib

--- wisely/macos/Flutter/GeneratedPluginRegistrant.swift
+++ wisely/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers
 import device_info_plus_macos
 import location
 import path_provider_macos
@@ -12,6 +13,7 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
