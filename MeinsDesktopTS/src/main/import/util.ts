b2b1d7c0d7ae5cd39752a0c65352f47d13cf5b34
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-10-06 20:09:04 +0200

    cleanup: remove unused code

--- MeinsDesktopTS/src/main/import/util.ts
+++
@@ -1,6 +0,0 @@
-// from https://gist.github.com/Atinux/fd2bcce63e44a7d3addddc166ce93fb2
-export async function asyncForEach(array: any[], callback: Function) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array)
-  }
-}
