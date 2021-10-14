81065eafd0bc797abcd0cd66c264992ecd47d15a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-10-14 13:50:38 +0200

    test: validate parsed entry against spec

--- src/test/meins/shared/import_test.cljs
+++ src/test/meins/shared/import_test.cljs
@@ -1,6 +1,8 @@
 (ns meins.shared.import-test
   (:require [cljs.test :refer [deftest testing is]]
-            [meins.electron.main.import :as emi]))
+            [meins.electron.main.import :as emi]
+            [meins.common.specs]
+            [cljs.spec.alpha :as s]))
 
 (def test-entry
   {:mentions   #{}
@@ -16,10 +18,11 @@
    :vclock     {"1231bb84-da9b-4abe-b0ab-b300349818af" 28}})
 
 (deftest read-entry-test
-  (let []
+  (let [parsed (emi/read-entry "./src/test/meins/shared/audio/2021-10-12/2021-10-12_15-11-43-702.aac.json")]
     (testing "JSON is parsed correctly"
-      (is (= (emi/read-entry "./src/test/meins/shared/audio/2021-10-12/2021-10-12_15-11-43-702.aac.json")
-             test-entry)))))
+      (is (= parsed test-entry)))
+    (testing "Parsed entry is valid"
+      (s/valid? :meins.entry/spec parsed))))
 
 (deftest list-dir-test
   (let []
