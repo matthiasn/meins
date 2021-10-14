4e2dfae6583246d91b723ac7fe1f331e5c14a5bf
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-10-15 00:31:24 +0200

    feat: add time recording entry for audio note

--- src/cljs/meins/electron/main/import.cljs
+++ src/cljs/meins/electron/main/import.cljs
@@ -10,12 +10,15 @@
 (def data-path (:data-path rt/runtime-info))
 (def audio-path (:audio-path rt/runtime-info))
 
-(defn read-entry [file]
+(defn parse-json [file]
   (let [json (.parse js/JSON (readFileSync file))
-        data (js->clj json)
-        entry {:timestamp  (get data "timestamp")
-               :md         ""
-               :text       ""
+        data (js->clj json)]
+    data))
+
+(defn convert-entry [data]
+  (let [entry {:timestamp  (get data "timestamp")
+               :md         "import audio"
+               :text       "import audio"
                :mentions   #{}
                :utc-offset (get data "utcOffset")
                :audio_file (get data "audioFile")
@@ -27,10 +30,29 @@
                :vclock     (get data "vectorClock")}]
     entry))
 
-(defn list-dir [path read-fn put-fn]
+(defn time-recording-entry [data]
+  (let [entry (convert-entry data)
+        entry-ts (:timestamp entry)
+        subentry (select-keys entry [:utc-offset
+                                     :timezone
+                                     :longitude
+                                     :latitude])
+        comment (merge subentry
+                       {:timestamp      (+ entry-ts 1000)
+                        :entry_type     :pomodoro
+                        :comment_for    entry-ts
+                        :text           "recording"
+                        :md             "- recording"
+                        :completed_time (/ (get data "duration")
+                                           1000000)})]
+    comment))
+
+(defn list-dir [path put-fn]
   (let [files (sync (str path "/**/*.json"))]
     (doseq [json-file files]
-      (let [entry (read-fn json-file)
+      (let [data (parse-json json-file)
+            entry (convert-entry data)
+            comment (time-recording-entry data)
             file (str/replace json-file ".json" "")
             audio-file (:audio_file entry)
             audio-file-path (str audio-path "/" audio-file)]
@@ -39,12 +61,15 @@
             (copyFileSync file audio-file-path)))
         (when (s/valid? :meins.entry/spec entry)
           (pp/pprint entry)
-          (put-fn [:entry/update entry]))))))
+          (put-fn [:entry/update entry]))
+        (when (s/valid? :meins.entry/spec comment)
+          (pp/pprint comment)
+          (put-fn [:entry/update comment]))))))
 
 (defn import-audio [{:keys [msg-payload put-fn]}]
   (let [path (:directory msg-payload)]
     (info "import-audio:" path)
-    (list-dir path read-entry put-fn)))
+    (list-dir path put-fn)))
 
 (defn cmp-map [cmp-id]
   {:cmp-id      cmp-id
