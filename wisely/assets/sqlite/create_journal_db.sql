631a25e39060d3d34f0eb983a4df651baec933c1
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-11-03 19:58:42 +0100

    journal database schema

---
+++ wisely/assets/sqlite/create_journal_db.sql
@@ -0,0 +1,39 @@
+
+CREATE TABLE "journal" (
+  "id" TEXT NOT NULL,
+  "created_at" INTEGER NOT NULL,
+  "updated_at" INTEGER NOT NULL,
+  "start_time" INTEGER NOT NULL,
+  "end_time" INTEGER NOT NULL,
+  "type" TEXT NOT NULL,
+  "subtype" TEXT,
+  "serialized" TEXT NOT NULL,
+  "schema_version" INTEGER NOT NULL DEFAULT 0,
+  "plain_text" TEXT,
+  "latitude" REAL,
+  "longitude" REAL,
+  "geohash_string" TEXT,
+  "geohash_int" INTEGER,
+  PRIMARY KEY ("id")
+);
+
+CREATE INDEX idx_journal_created_at
+ON journal (created_at);
+
+CREATE INDEX idx_journal_updated_at
+ON journal (updated_at);
+
+CREATE INDEX idx_journal_start_time
+ON journal (start_time);
+
+CREATE INDEX idx_journal_type
+ON journal (type);
+
+CREATE INDEX idx_journal_subtype
+ON journal (subtype);
+
+CREATE INDEX idx_journal_geohash_string
+ON journal (geohash_string);
+
+CREATE INDEX idx_journal_geohash_int
+ON journal (geohash_int);
