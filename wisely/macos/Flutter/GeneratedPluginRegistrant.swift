8da2fdb0703f16fbe3e8a1167bde433a2ec4b426
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-11-06 00:26:17 +0100

    fix image access on macOS

--- wisely/macos/Flutter/GeneratedPluginRegistrant.swift
+++ wisely/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,8 +12,10 @@ import flutter_native_timezone
 import flutter_secure_storage_macos
 import just_audio
 import location
+import package_info_plus_macos
 import path_provider_macos
 import photo_manager
+import sentry_flutter
 import sqflite
 import url_launcher_macos
 
@@ -25,8 +27,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ImageScannerPlugin.register(with: registry.registrar(forPlugin: "ImageScannerPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
