d5204ab92a8feb22eef0b652b523d68ef2f6bb1a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-11-06 02:53:22 +0100

    disable connection status check, not currently working on macOS

--- wisely/lib/blocs/sync/outbound_queue_cubit.dart
+++ wisely/lib/blocs/sync/outbound_queue_cubit.dart
@@ -57,7 +57,11 @@ class OutboundQueueCubit extends Cubit<OutboundQueueState> {
 
   void sendNext() async {
     var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult != ConnectivityResult.none && !sendMutex.isLocked) {
+    debugPrint('sendNext connectivityResult $connectivityResult');
+    // TODO: check why no working on macOS
+    //bool isConnected = connectivityResult != ConnectivityResult.none;
+    bool isConnected = true;
+    if (isConnected && !sendMutex.isLocked) {
       List<OutboundQueueRecord> unprocessed = await _db.oldestEntries();
       if (unprocessed.isNotEmpty) {
         sendMutex.acquire();
