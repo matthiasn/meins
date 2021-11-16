d40cdd8aa329a6b442389687d80ba7ca01ab9148
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-11-16 01:40:44 +0100

    format bold and italic via keyboard

--- wisely/lib/pages/editor.dart
+++ wisely/lib/pages/editor.dart
@@ -75,34 +75,56 @@ class EditorWidget extends StatelessWidget {
 
   final QuillController _controller;
 
+  void keyFormatter(RawKeyEvent event, String char, Attribute attribute) {
+    if (event.data.isMetaPressed && event.character == char) {
+      if (_controller
+          .getSelectionStyle()
+          .attributes
+          .keys
+          .contains(attribute.key)) {
+        _controller.formatSelection(Attribute.clone(attribute, null));
+      } else {
+        _controller.formatSelection(attribute);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        height: 300,
-        color: AppColors.editorBgColor,
-        child: Column(
-          children: [
-            QuillToolbar.basic(
-              controller: _controller,
-              showColorButton: false,
-              showBackgroundColorButton: false,
-              showListCheck: false,
-              showIndent: false,
-              showQuote: false,
-              showSmallButton: false,
-              showImageButton: false,
-              showLink: false,
-            ),
-            Expanded(
-              child: QuillEditor.basic(
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      onKey: (RawKeyEvent event) {
+        keyFormatter(event, 'b', Attribute.bold);
+        keyFormatter(event, 'i', Attribute.italic);
+      },
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          height: 300,
+          color: AppColors.editorBgColor,
+          child: Column(
+            children: [
+              QuillToolbar.basic(
                 controller: _controller,
-                readOnly: false, // true for view only mode
+                showColorButton: false,
+                showBackgroundColorButton: false,
+                showListCheck: false,
+                showIndent: false,
+                showQuote: false,
+                showSmallButton: false,
+                showImageButton: false,
+                showLink: false,
+                showUnderLineButton: false,
               ),
-            )
-          ],
+              Expanded(
+                child: QuillEditor.basic(
+                  controller: _controller,
+                  readOnly: false, // true for view only mode
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
