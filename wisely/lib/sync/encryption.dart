4c31650bb4dc8b82e2f5a91ad51c3e6d5db1010a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-11-22 02:09:00 +0100

    run file encryption and decryption in isolates

--- wisely/lib/sync/encryption.dart
+++ wisely/lib/sync/encryption.dart
@@ -7,19 +7,18 @@ import 'package:flutter/foundation.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:wisely/sync/encryption_messages.dart';
 
-Future<void> encryptFile(
-    File inputFile, File encryptedFile, String b64Secret) async {
+FutureOr<void> encryptFileIsolate(EncryptFileMessage msg) async {
   final transaction = Sentry.startTransaction('encryptFile()', 'task');
 
-  if (!inputFile.existsSync()) {
+  if (!msg.inputFile.existsSync()) {
     debugPrint('File does not exist, aborting');
     throw Exception("File not found");
   }
 
-  final List<int> message = await inputFile.readAsBytes();
+  final List<int> message = await msg.inputFile.readAsBytes();
   final algorithm = AesGcm.with256bits();
   final secretKey =
-      await algorithm.newSecretKeyFromBytes(base64Decode(b64Secret));
+      await algorithm.newSecretKeyFromBytes(base64Decode(msg.b64Secret));
   final nonce = algorithm.newNonce();
 
   final SecretBox secretBox = await algorithm.encrypt(
@@ -28,42 +27,62 @@ Future<void> encryptFile(
     nonce: nonce,
   );
 
-  await encryptedFile.writeAsBytes(secretBox.concatenation());
+  await msg.encryptedFile.writeAsBytes(secretBox.concatenation());
   await transaction.finish();
 }
 
-Future<void> decryptFile(
-    File inputFile, File outputFile, String b64Secret) async {
+Future<void> encryptFile(
+    File inputFile, File encryptedFile, String b64Secret) async {
+  return compute(
+      encryptFileIsolate,
+      EncryptFileMessage(
+        b64Secret: b64Secret,
+        inputFile: inputFile,
+        encryptedFile: encryptedFile,
+      ));
+}
+
+FutureOr<void> decryptFileIsolate(DecryptFileMessage msg) async {
   final transaction = Sentry.startTransaction('decryptFile()', 'task');
 
-  if (!inputFile.existsSync()) {
+  if (!msg.inputFile.existsSync()) {
     debugPrint('File does not exist, aborting');
     throw Exception("File not found");
   }
 
   final algorithm = AesGcm.with256bits();
-  final List<int> bytes = await inputFile.readAsBytes();
+  final List<int> bytes = await msg.inputFile.readAsBytes();
   final deserializedSecretBox =
       SecretBox.fromConcatenation(bytes, nonceLength: 12, macLength: 16);
   final secretKey =
-      await algorithm.newSecretKeyFromBytes(base64Decode(b64Secret));
+      await algorithm.newSecretKeyFromBytes(base64Decode(msg.b64Secret));
 
   final List<int> decryptedBytes = await algorithm.decrypt(
     deserializedSecretBox,
     secretKey: secretKey,
   );
 
-  await outputFile.writeAsBytes(decryptedBytes);
+  await msg.decryptedFile.writeAsBytes(decryptedBytes);
   await transaction.finish();
 }
 
-Future<String> encryptStringIsolate(
-    EncryptStringMessage encryptStringMessage) async {
-  final transaction = Sentry.startTransaction('encryptString()', 'task');
-  final List<int> message = utf8.encode(encryptStringMessage.plaintext);
+Future<void> decryptFile(
+    File inputFile, File decryptedFile, String b64Secret) async {
+  return compute(
+      decryptFileIsolate,
+      DecryptFileMessage(
+        b64Secret: b64Secret,
+        inputFile: inputFile,
+        decryptedFile: decryptedFile,
+      ));
+}
+
+Future<String> encryptStringIsolate(EncryptStringMessage msg) async {
+  final transaction = Sentry.startTransaction('encryptStringIsolate()', 'task');
+  final List<int> message = utf8.encode(msg.plaintext);
   final algorithm = AesGcm.with256bits();
-  final secretKey = await algorithm
-      .newSecretKeyFromBytes(base64Decode(encryptStringMessage.b64Secret));
+  final secretKey =
+      await algorithm.newSecretKeyFromBytes(base64Decode(msg.b64Secret));
   final nonce = algorithm.newNonce();
 
   final SecretBox secretBox = await algorithm.encrypt(
@@ -85,16 +104,15 @@ Future<String> encryptString(String plainText, b64Secret) async {
   );
 }
 
-FutureOr<String> decryptStringIsolate(
-    DecryptStringMessage decryptStringMessage) async {
-  final transaction = Sentry.startTransaction('decryptString()', 'task');
+FutureOr<String> decryptStringIsolate(DecryptStringMessage msg) async {
+  final transaction = Sentry.startTransaction('decryptStringIsolate()', 'task');
 
   final algorithm = AesGcm.with256bits();
-  final List<int> bytes = base64.decode(decryptStringMessage.encrypted);
+  final List<int> bytes = base64.decode(msg.encrypted);
   final deserializedSecretBox =
       SecretBox.fromConcatenation(bytes, nonceLength: 12, macLength: 16);
-  final secretKey = await algorithm
-      .newSecretKeyFromBytes(base64Decode(decryptStringMessage.b64Secret));
+  final secretKey =
+      await algorithm.newSecretKeyFromBytes(base64Decode(msg.b64Secret));
 
   final List<int> decryptedBytes = await algorithm.decrypt(
     deserializedSecretBox,
--- wisely/lib/sync/encryption_messages.dart
+++ wisely/lib/sync/encryption_messages.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'encryption_messages.freezed.dart';
+
+@freezed
+class EncryptStringMessage with _$EncryptStringMessage {
+  factory EncryptStringMessage({
+    required String plaintext,
+    required String b64Secret,
+  }) = _EncryptStringMessage;
+}
 
 @freezed
 class DecryptStringMessage with _$DecryptStringMessage {
@@ -11,9 +21,19 @@ class DecryptStringMessage with _$DecryptStringMessage {
 }
 
 @freezed
-class EncryptStringMessage with _$EncryptStringMessage {
-  factory EncryptStringMessage({
-    required String plaintext,
+class EncryptFileMessage with _$EncryptFileMessage {
+  factory EncryptFileMessage({
     required String b64Secret,
-  }) = _EncryptStringMessage;
+    required File inputFile,
+    required File encryptedFile,
+  }) = _EncryptFileMessage;
+}
+
+@freezed
+class DecryptFileMessage with _$DecryptFileMessage {
+  factory DecryptFileMessage({
+    required String b64Secret,
+    required File inputFile,
+    required File decryptedFile,
+  }) = _DecryptFileMessage;
 }
