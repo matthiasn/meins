413010e6028755a8b7c537297ca401513c12b061
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-12-03 21:45:40 +0100

    use drift file for table definitions

---
+++ lotti/lib/drift_db/journal_db.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'journal_db.g.dart';
+
+@DriftDatabase(
+  include: {'journal_tables.drift'},
+)
+class JournalDb extends _$JournalDb {
+  JournalDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
