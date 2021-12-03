f2d3fc11408a95302fc9119246ec88577d2eabb8
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-12-03 18:25:27 +0100

    journal table definition

--- lotti/lib/drift_db/database.dart
+++ lotti/lib/drift_db/database.dart
@@ -7,6 +7,36 @@ import 'package:path_provider/path_provider.dart';
 
 part 'database.g.dart';
 
+@DataClassName('JournalEntry')
+class Journal extends Table {
+  TextColumn get id => text()();
+
+  DateTimeColumn get createdAt =>
+      dateTime().named('created_at').withDefault(Constant(DateTime.now()))();
+
+  DateTimeColumn get updatedAt =>
+      dateTime().named('updated_at').withDefault(Constant(DateTime.now()))();
+
+  DateTimeColumn get dateFrom => dateTime().named('date_from')();
+  DateTimeColumn get dateTo => dateTime().named('date_to')();
+
+  TextColumn get serialized => text()();
+  TextColumn get plainText => text().named('plain_text').nullable()();
+
+  TextColumn get type => text()();
+  TextColumn get subType => text()();
+
+  IntColumn get schemaVersion => integer().withDefault(const Constant(0))();
+
+  RealColumn get latitude => real()();
+  RealColumn get longitude => real()();
+  IntColumn get geohashInt => integer().named('geohash_int').nullable()();
+  TextColumn get geohashString => text().named('geohash_string')();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
 class Conflicts extends Table {
   TextColumn get id => text()();
   DateTimeColumn get createdAt => dateTime().named('created_at')();
