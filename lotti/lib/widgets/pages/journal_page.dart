35cdcb68ccab2a7e818ee1105e93e56cf6aa9cd3
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-12-06 03:21:57 +0100

    add multi select for journal item type

--- lotti/lib/widgets/pages/journal_page.dart
+++ lotti/lib/widgets/pages/journal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lotti/blocs/journal/persistence_cubit.dart';
@@ -6,6 +7,8 @@ import 'package:lotti/blocs/journal/persistence_state.dart';
 import 'package:lotti/classes/journal_entities.dart';
 import 'package:lotti/theme.dart';
 import 'package:lotti/widgets/journal/journal_card.dart';
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+import 'package:multi_select_flutter/util/multi_select_item.dart';
 
 class JournalPage extends StatefulWidget {
   const JournalPage({
@@ -21,7 +24,27 @@ class JournalPage extends StatefulWidget {
   _JournalPageState createState() => _JournalPageState();
 }
 
+class FilterBy {
+  final String typeName;
+  final String name;
+
+  FilterBy({
+    required this.typeName,
+    required this.name,
+  });
+}
+
 class _JournalPageState extends State<JournalPage> {
+  static final List<FilterBy> _entryTypes = [
+    FilterBy(typeName: r'_$JournalEntry', name: "Text"),
+    FilterBy(typeName: r'_$JournalAudio', name: "Audio"),
+    FilterBy(typeName: r'_$JournalImage', name: "Photo"),
+    FilterBy(typeName: r'_$QuantitativeEntry', name: "Quantitative"),
+  ];
+  final _items = _entryTypes
+      .map((entryType) => MultiSelectItem<FilterBy?>(entryType, entryType.name))
+      .toList();
+
   @override
   void initState() {
     super.initState();
@@ -40,7 +63,41 @@ class _JournalPageState extends State<JournalPage> {
                 return Scaffold(
                   appBar: AppBar(
                     backgroundColor: AppColors.headerBgColor,
-                    title: widget.child,
+                    title: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      child: MultiSelectDialogField(
+                        items: _items,
+                        title: const Text("Entry Types"),
+                        selectedColor: Colors.blue,
+                        decoration: BoxDecoration(
+                          color: Colors.blue.withOpacity(0.1),
+                          borderRadius:
+                              BorderRadius.all(const Radius.circular(40)),
+                          border: Border.all(
+                            color: AppColors.entryBgColor,
+                            width: 2,
+                          ),
+                        ),
+                        buttonIcon: Icon(
+                          Icons.search,
+                          color: AppColors.entryBgColor,
+                        ),
+                        buttonText: Text(
+                          "Filter by Type",
+                          style: TextStyle(
+                            color: AppColors.entryBgColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                        onConfirm: (List<FilterBy?> results) {
+                          debugPrint(results.toString());
+                          context.read<PersistenceCubit>().queryFilteredJournal(
+                              results
+                                  .map((FilterBy? e) => e!.typeName)
+                                  .toList());
+                        },
+                      ),
+                    ),
                     centerTitle: true,
                   ),
                   backgroundColor: AppColors.bodyBgColor,
