47b2e38707b2cba65afbe2580f6768de70a6827f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2021-12-07 21:24:30 +0100

    use stream query for measurable type, insert some

--- lotti/lib/classes/measurables.dart
+++ lotti/lib/classes/measurables.dart
@@ -11,41 +11,10 @@ class MeasurableDataType with _$MeasurableDataType {
     required DateTime updatedAt,
     required String uniqueName,
     required String displayName,
-    required MeasurableUnit unit,
-    required MeasurableType measurableType,
+    required String unitName,
     required int version,
   }) = _MeasurableDataType;
 
   factory MeasurableDataType.fromJson(Map<String, dynamic> json) =>
       _$MeasurableDataTypeFromJson(json);
-}
-
-@freezed
-class MeasurableUnit with _$MeasurableUnit {
-  factory MeasurableUnit({
-    required String id,
-    required DateTime createdAt,
-    required DateTime updatedAt,
-    required String unit,
-    required String displayUnit,
-    required int version,
-  }) = _MeasurableUnit;
-
-  factory MeasurableUnit.fromJson(Map<String, dynamic> json) =>
-      _$MeasurableUnitFromJson(json);
-}
-
-@freezed
-class MeasurableType with _$MeasurableType {
-  factory MeasurableType({
-    required String id,
-    required DateTime createdAt,
-    required DateTime updatedAt,
-    required String uniqueName,
-    required String displayName,
-    required int version,
-  }) = _MeasurableType;
-
-  factory MeasurableType.fromJson(Map<String, dynamic> json) =>
-      _$MeasurableTypeFromJson(json);
 }
--- lotti/lib/widgets/pages/measurables.dart
+++ lotti/lib/widgets/pages/measurables.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lotti/classes/measurables.dart';
+import 'package:lotti/database/database.dart';
+import 'package:lotti/main.dart';
 import 'package:lotti/theme.dart';
+import 'package:units_converter/units_converter.dart';
 
 class MeasurablesPage extends StatefulWidget {
   const MeasurablesPage({Key? key}) : super(key: key);
@@ -9,9 +13,50 @@ class MeasurablesPage extends StatefulWidget {
 }
 
 class _MeasurablesPageState extends State<MeasurablesPage> {
+  final JournalDb _db = getIt<JournalDb>();
+
+  late final Stream<List<MeasurableDataType>> stream;
+
   @override
   void initState() {
     super.initState();
+
+    var length = Length()..convert(LENGTH.meters, 1);
+    Unit unit = length.inches;
+    print(
+        'name:${unit.name}, value:${unit.value}, stringValue:${unit.stringValue}, symbol:${unit.symbol}');
+
+    stream = _db.watchMeasurableDataTypes();
+
+    stream.listen((List<MeasurableDataType> data) {
+      debugPrint('DataReceived: $data');
+    }, onDone: () {
+      debugPrint('Task Done');
+    }, onError: (error) {
+      debugPrint('Some Error');
+    });
+
+    DateTime now = DateTime.now();
+
+    _db.addMeasurable(MeasurableDataType(
+      id: '9e9e7a62-1e56-4059-a568-12234db7399b',
+      createdAt: now,
+      updatedAt: now,
+      uniqueName: 'water',
+      displayName: 'Water',
+      unitName: 'ml',
+      version: 0,
+    ));
+
+    _db.addMeasurable(MeasurableDataType(
+      id: 'f2518f33-af1d-4dbe-ae9b-6a05def5d8f9',
+      createdAt: now,
+      updatedAt: now,
+      uniqueName: 'caffeine',
+      displayName: 'Caffeine',
+      unitName: 'mg',
+      version: 0,
+    ));
   }
 
   @override
