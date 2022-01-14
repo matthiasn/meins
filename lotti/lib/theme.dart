fac853b4f6ad2da83a35bbc47f45e703ad997495
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-01-14 01:57:10 -0500

    Show starred and private status in journal cards

--- lotti/lib/theme.dart
+++ lotti/lib/theme.dart
@@ -6,6 +6,7 @@ class AppColors {
   static Color entryBgColor = const Color.fromRGBO(155, 200, 245, 1);
   static Color entryTextColor = const Color.fromRGBO(158, 158, 158, 1);
   static Color editorTextColor = const Color.fromRGBO(51, 51, 51, 1);
+  static Color starredGold = const Color.fromRGBO(255, 215, 0, 1);
   static Color editorBgColor = Colors.white;
 
   static Color headerBgColor = const Color.fromRGBO(68, 68, 85, 1);
