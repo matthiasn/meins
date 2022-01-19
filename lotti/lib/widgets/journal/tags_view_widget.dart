0c966e1a1cdc2b115f0be2e677fcb891787dbcd5
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-01-19 18:50:41 -0500

    Tag lookup from names and ids

--- lotti/lib/widgets/journal/tags_view_widget.dart
+++ lotti/lib/widgets/journal/tags_view_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lotti/classes/entity_definitions.dart';
 import 'package:lotti/classes/journal_entities.dart';
 import 'package:lotti/main.dart';
 import 'package:lotti/services/tags_service.dart';
@@ -16,94 +17,112 @@ class TagsViewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> tags = item.meta.tags ?? [];
+    List<String> tagNames = item.meta.tags ?? [];
     List<String> tagIds = item.meta.tagIds ?? [];
+    List<TagDefinition> tagsFromTagNames = [];
+    List<TagDefinition> tagsFromTagIds = [];
+
+    for (String tagName in tagNames) {
+      TagDefinition? tagDefinition = tagsService.getTagByName(tagName);
+      if (tagDefinition != null) {
+        tagsFromTagNames.add(tagDefinition);
+      }
+    }
+    for (String tagId in tagIds) {
+      TagDefinition? tagDefinition = tagsService.getTagById(tagId);
+      if (tagDefinition != null) {
+        tagsFromTagIds.add(tagDefinition);
+      }
+    }
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 2.0),
       child: Column(
         children: [
           Wrap(
-              spacing: 3,
-              runSpacing: 2,
-              children: tags
-                  .map(
-                    (String tag) => Padding(
-                      padding: const EdgeInsets.only(bottom: 1.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(4),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 1,
-                            horizontal: 4,
-                          ),
-                          color: Colors.white,
-                          child: Text(
-                            tag,
-                            style: const TextStyle(
-                              fontSize: 10,
-                              fontFamily: 'Oswald',
-                            ),
+            spacing: 3,
+            runSpacing: 2,
+            children: tagNames
+                .map(
+                  (String tag) => Padding(
+                    padding: const EdgeInsets.only(bottom: 1.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 1,
+                          horizontal: 4,
+                        ),
+                        color: Colors.white,
+                        child: Text(
+                          tag,
+                          style: const TextStyle(
+                            fontSize: 10,
+                            fontFamily: 'Oswald',
                           ),
                         ),
                       ),
                     ),
-                  )
-                  .toList()),
+                  ),
+                )
+                .toList(),
+          ),
           Wrap(
-              spacing: 3,
-              runSpacing: 2,
-              children: tags
-                  .map(
-                    (String tag) => Padding(
-                      padding: const EdgeInsets.only(bottom: 1.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(4),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 1,
-                            horizontal: 4,
-                          ),
-                          color: AppColors.error,
-                          child: Text(
-                            tagsService.getTagByName(tag)?.tag ?? '---',
-                            style: const TextStyle(
-                              fontSize: 10,
-                              fontFamily: 'Oswald',
-                            ),
+            spacing: 3,
+            runSpacing: 2,
+            children: tagsFromTagNames
+                .map(
+                  (TagDefinition tagDefinition) => Padding(
+                    padding: const EdgeInsets.only(bottom: 1.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 1,
+                          horizontal: 4,
+                        ),
+                        color: AppColors.error,
+                        child: Text(
+                          tagDefinition.tag,
+                          style: const TextStyle(
+                            fontSize: 10,
+                            fontFamily: 'Oswald',
                           ),
                         ),
                       ),
                     ),
-                  )
-                  .toList()),
+                  ),
+                )
+                .toList(),
+          ),
           Wrap(
-              spacing: 3,
-              runSpacing: 2,
-              children: tags
-                  .map(
-                    (String tag) => Padding(
-                      padding: const EdgeInsets.only(bottom: 1.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(4),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 1,
-                            horizontal: 4,
-                          ),
-                          color: AppColors.entryBgColor,
-                          child: Text(
-                            tagsService.getTagByName(tag)?.tag ?? '---',
-                            style: const TextStyle(
-                              fontSize: 10,
-                              fontFamily: 'Oswald',
-                            ),
+            spacing: 3,
+            runSpacing: 2,
+            children: tagsFromTagIds
+                .map(
+                  (TagDefinition tagDefinition) => Padding(
+                    padding: const EdgeInsets.only(bottom: 1.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 1,
+                          horizontal: 4,
+                        ),
+                        color: AppColors.entryBgColor,
+                        child: Text(
+                          tagDefinition.tag,
+                          style: const TextStyle(
+                            fontSize: 10,
+                            fontFamily: 'Oswald',
                           ),
                         ),
                       ),
                     ),
-                  )
-                  .toList()),
+                  ),
+                )
+                .toList(),
+          ),
         ],
       ),
     );
