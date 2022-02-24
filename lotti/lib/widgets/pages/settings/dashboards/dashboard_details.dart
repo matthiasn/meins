42edbc6e49e6be48760cd3d209391a2bdf3ddf8b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-02-24 01:35:23 +0100

    Dashboard item card label from type config

--- lotti/lib/widgets/pages/settings/dashboards/dashboard_details.dart
+++ lotti/lib/widgets/pages/settings/dashboards/dashboard_details.dart
@@ -7,6 +7,7 @@ import 'package:lotti/database/database.dart';
 import 'package:lotti/get_it.dart';
 import 'package:lotti/logic/persistence_logic.dart';
 import 'package:lotti/theme.dart';
+import 'package:lotti/widgets/charts/dashboard_health_data.dart';
 import 'package:lotti/widgets/misc/buttons.dart';
 import 'package:lotti/widgets/pages/settings/form_text_field.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -506,7 +507,11 @@ class DashboardItemCard extends StatelessWidget {
         }
         return '';
       },
-      healthChart: (healthLineChart) => healthLineChart.healthType,
+      healthChart: (healthLineChart) {
+        String type = healthLineChart.healthType;
+        String itemName = healthTypes[type]?.displayName ?? type;
+        return itemName;
+      },
     );
 
     return Card(
