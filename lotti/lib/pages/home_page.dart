9168fb86528b16d06d106bc2fb00e6b1fd5352a9
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-03-08 13:40:07 +0100

    Transition duration tweak

--- lotti/lib/pages/home_page.dart
+++ lotti/lib/pages/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AutoTabsScaffold(
+      animationDuration: const Duration(milliseconds: 500),
       appBarBuilder: (context, tabsRouter) => AppBar(
         backgroundColor: AppColors.headerBgColor,
         title: const VersionAppBar(title: 'Lotti'),
