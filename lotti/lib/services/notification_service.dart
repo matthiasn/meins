437d0e6ef79f1ac6cfa69e8d33fdcfaacd60e1cc
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-03-21 23:48:09 +0100

    Add notification scheduling function

--- lotti/lib/services/notification_service.dart
+++ lotti/lib/services/notification_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:lotti/database/database.dart';
 import 'package:lotti/get_it.dart';
+import 'package:timezone/standalone.dart' as tz;
 
 final JournalDb _db = getIt<JournalDb>();
 
@@ -97,5 +98,55 @@ class NotificationService {
         ),
       );
     }
+  }
+
+  Future<void> scheduleNotification({
+    required String title,
+    required String body,
+    required DateTime notifyAt,
+    required notificationId,
+    String? deepLink,
+  }) async {
+    bool notifyEnabled = await _db.getConfigFlag('enable_notifications');
+
+    if (Platform.isWindows || Platform.isLinux) {
+      return;
+    }
+
+    await _requestPermissions();
+    flutterLocalNotificationsPlugin.cancel(notificationId);
+
+    DateTime now = DateTime.now();
+    await tz.initializeTimeZone();
+
+    tz.TZDateTime scheduledDate = tz.TZDateTime(
+      tz.getLocation('Europe/Berlin'),
+      now.year,
+      now.month,
+      now.day,
+      notifyAt.hour,
+      notifyAt.minute,
+    );
+
+    flutterLocalNotificationsPlugin.zonedSchedule(
+      notificationId,
+      title,
+      body,
+      scheduledDate,
+      const NotificationDetails(
+        iOS: IOSNotificationDetails(
+          presentAlert: true,
+          presentSound: true,
+        ),
+        macOS: MacOSNotificationDetails(
+          presentAlert: true,
+          presentSound: true,
+        ),
+      ),
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.wallClockTime,
+      androidAllowWhileIdle: true,
+      matchDateTimeComponents: DateTimeComponents.time,
+    );
   }
 }
