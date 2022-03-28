ddeecae0f193eb7f056af1b2fb85b656369bf67c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-03-28 15:07:21 +0200

    Copy story tags when linking

--- lotti/lib/services/link_service.dart
+++ lotti/lib/services/link_service.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/services.dart';
+import 'package:lotti/database/database.dart';
 import 'package:lotti/get_it.dart';
 import 'package:lotti/logic/persistence_logic.dart';
+import 'package:lotti/services/tags_service.dart';
 
 class LinkService {
   final PersistenceLogic _persistenceLogic = getIt<PersistenceLogic>();
+  final TagsService _tagsService = getIt<TagsService>();
+  final JournalDb _journalDb = getIt<JournalDb>();
 
   String? _linkToId;
   String? _linkFromId;
@@ -15,6 +19,16 @@ class LinkService {
       await _persistenceLogic.createLink(
         fromId: _linkFromId!,
         toId: _linkToId!,
+      );
+
+      final linkedFrom = await _journalDb.journalEntityById(_linkFromId!);
+      List<String>? linkedTagIds = linkedFrom?.meta.tagIds;
+      List<String> storyTags =
+          _tagsService.getFilteredStoryTagIds(linkedTagIds);
+
+      _persistenceLogic.addTags(
+        journalEntityId: _linkToId!,
+        addedTagIds: storyTags,
       );
 
       Future.delayed(const Duration(minutes: 2)).then((_) {
