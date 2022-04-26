06ff9a0f1afbb46868d688060780e63d975cbe2b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-04-26 23:00:54 +0200

    Get timezone name on Linux

--- lotti/lib/utils/timezone.dart
+++ lotti/lib/utils/timezone.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 
 Future<String> getLocalTimezone() async {
+  DateTime now = DateTime.now();
+
   return (!Platform.isWindows && !Platform.isLinux)
       ? await FlutterNativeTimezone.getLocalTimezone()
-      : '';
+      : now.timeZoneName;
 }
