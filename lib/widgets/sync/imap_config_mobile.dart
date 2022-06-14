4d79184ad13a6b3cd63d3f17abfc1615ffc04d95
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-06-14 20:39:07 +0200

    Refactor & cleanup

---
+++ lib/widgets/sync/imap_config_mobile.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lotti/blocs/sync/sync_config_cubit.dart';
+import 'package:lotti/theme.dart';
+import 'package:lotti/widgets/sync/qr_reader_widget.dart';
+import 'package:lotti/widgets/sync/qr_widget.dart';
+
+class MobileSyncConfig extends StatelessWidget {
+  const MobileSyncConfig({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SyncConfigCubit, SyncConfigState>(
+        builder: (context, SyncConfigState state) {
+      return Center(
+        child: state.maybeWhen(
+          (sharedKey, imapConfig) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 88.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Host: ${imapConfig?.host}',
+                    style: labelStyleLarger,
+                  ),
+                  Text(
+                    'Port: ${imapConfig?.port}',
+                    style: labelStyleLarger,
+                  ),
+                  Text(
+                    'IMAP Folder: ${imapConfig?.folder}',
+                    style: labelStyleLarger,
+                  ),
+                  Text(
+                    'User: ${imapConfig?.userName}',
+                    style: labelStyleLarger,
+                  ),
+                  const SizedBox(height: 32),
+                  const DeleteSyncConfigButton(),
+                ],
+              ),
+            );
+          },
+          orElse: () {
+            return const EncryptionQrReaderWidget();
+          },
+        ),
+      );
+    });
+  }
+}
