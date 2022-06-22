9f3c2f8c9f743325734ef0ee821398eca056b573
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-06-23 01:30:51 +0200

    Remove leading insights icon in measurement card

--- lib/widgets/journal/journal_card.dart
+++ lib/widgets/journal/journal_card.dart
@@ -177,7 +177,6 @@ class JournalCard extends StatelessWidget {
               journalAudio: (_) => const LeadingIcon(Icons.mic),
               journalEntry: (_) => const LeadingIcon(Icons.article),
               quantitative: (_) => const LeadingIcon(MdiIcons.heart),
-              measurement: (_) => const LeadingIcon(Icons.insights),
               orElse: () => null,
             ),
             title: JournalCardTitle(item: item),
