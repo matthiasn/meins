dc64ee72d17b700aba0b6be08b5a8cd341c5ffd8
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-08-07 00:29:58 +0200

    Chore: upgrade maps lib

--- lib/map/cached_tile_provider.dart
+++ lib/map/cached_tile_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
 
 class CachedTileProvider extends TileProvider {
-  const CachedTileProvider();
+  CachedTileProvider();
   @override
   ImageProvider getImage(Coords<num> coords, TileLayerOptions options) {
     return CachedNetworkImageProvider(
