e40e7c254a204d30a273b07bcca7eeee29961ee8
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-08-17 16:15:03 +0200

    CI: new temp documents dir on each run

--- test/helpers/path_provider.dart
+++ test/helpers/path_provider.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lotti/utils/file_utils.dart';
 
 void setFakeDocumentsPath() {
+  final dir = uuid.v1();
   const MethodChannel('plugins.flutter.io/path_provider_macos')
       .setMockMethodCallHandler((MethodCall methodCall) async {
-    return '/tmp/lotti';
+    return '/tmp/$dir';
   });
 
   const MethodChannel('plugins.flutter.io/path_provider_linux')
       .setMockMethodCallHandler((MethodCall methodCall) async {
-    return '/tmp/lotti';
+    return '/tmp/$dir';
   });
 
   const MethodChannel('plugins.flutter.io/path_provider_windows')
       .setMockMethodCallHandler((MethodCall methodCall) async {
-    return '/tmp/lotti';
+    return '/tmp/$dir';
   });
 }
