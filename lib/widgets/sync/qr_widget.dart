df17144260e5ce180c788939585e7137540c2a75
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-08-18 00:19:48 +0200

    Fix closing dialog

--- lib/widgets/sync/qr_widget.dart
+++ lib/widgets/sync/qr_widget.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -140,8 +139,9 @@ class EncryptionQrWidget extends StatelessWidget {
                                       await Clipboard.setData(
                                         ClipboardData(text: encryptedConfig),
                                       );
-                                      await context.router.pop();
-                                      //                                Navigator.pop(context, 'Copy SyncConfig');
+
+                                      // ignore: use_build_context_synchronously
+                                      Navigator.pop(context, 'Copy SyncConfig');
                                       showPassphrase();
                                     },
                                     primaryColor: Colors.red,
