be90da6528f5ec18702f2949943d756893ed7f26
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-08-18 02:54:53 +0200

    Adapt Beamer app from example

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/localization/l10n.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
+import 'package:lotti/beamer_app.dart';
 import 'package:lotti/blocs/audio/player_cubit.dart';
 import 'package:lotti/blocs/audio/recorder_cubit.dart';
 import 'package:lotti/blocs/sync/outbox_cubit.dart';
@@ -51,7 +52,8 @@ Future<void> main() async {
 
     registerScreenshotHotkey();
 
-    runApp(LottiApp());
+    //runApp(LottiApp());
+    runApp(MyBeamerApp());
   }, (Object error, StackTrace stackTrace) {
     getIt<LoggingDb>().captureException(
       error,
