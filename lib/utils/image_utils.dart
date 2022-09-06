e9fe3cf24cbc696556e7b2ba6d3e1b248cb642f6
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-09-06 15:23:59 +0200

    Refactor: remove unused image utils

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -1,89 +1,9 @@
 import 'dart:io';
 
-import 'package:exif/exif.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:lotti/classes/journal_entities.dart';
 import 'package:path_provider/path_provider.dart';
-
-Future<void> printGeolocation(Uint8List fileBytes) async {
-  final data = await readExifFromBytes(fileBytes);
-
-  if (data.isEmpty) {
-    debugPrint('No EXIF information found');
-    return;
-  }
-
-  final latRef = data['GPS GPSLatitudeRef']?.toString();
-  var latVal = gpsValuesToFloat(data['GPS GPSLatitude']?.values);
-  final lngRef = data['GPS GPSLongitudeRef']?.toString();
-  var lngVal = gpsValuesToFloat(data['GPS GPSLongitude']?.values);
-
-  if (latRef == null || latVal == null || lngRef == null || lngVal == null) {
-    debugPrint('GPS information not found');
-    return;
-  }
-
-  if (latRef == 'S') {
-    latVal *= -1;
-  }
-
-  if (lngRef == 'W') {
-    lngVal *= -1;
-  }
-}
-
-double? gpsValuesToFloat(IfdValues? values) {
-  if (values == null || values is! IfdRatios) {
-    return null;
-  }
-
-  // ignore: omit_local_variable_types
-  double sum = 0;
-  // ignore: omit_local_variable_types
-  double unit = 1;
-
-  for (final v in values.ratios) {
-    sum += v.toDouble() * unit;
-    unit /= 60;
-  }
-
-  return sum;
-}
-
-Future<void> printExif(Uint8List bytes) async {
-  final data = await readExifFromBytes(bytes);
-
-  if (data.isEmpty) {
-    debugPrint('No EXIF information found');
-    return;
-  }
-
-  for (final entry in data.entries) {
-    debugPrint('${entry.key}: ${entry.value}');
-  }
-
-  final created = data['EXIF DateTimeOriginal']?.toString();
-  final offsetTime = data['EXIF OffsetTimeOriginal']?.toString();
-  debugPrint('Image created: $created $offsetTime');
-}
-
-Future<void> printExifFromFile(File file) async {
-  final data = await readExifFromFile(file);
-
-  if (data.isEmpty) {
-    debugPrint('No EXIF information found');
-    return;
-  }
-
-  for (final entry in data.entries) {
-    debugPrint('${entry.key}: ${entry.value}');
-  }
-
-  final created = data['EXIF DateTimeOriginal']?.toString();
-  final offsetTime = data['EXIF OffsetTimeOriginal']?.toString();
-  debugPrint('Image created: $created $offsetTime');
-}
 
 Future<File?> compressAndGetFile(File file) async {
   final sourcePath = file.absolute.path;
