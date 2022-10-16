e0a5f80849cc90dec6bbc8b935ff8cb280054b53
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-10-17 01:19:20 +0200

    Fix habit success indicator width

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Fixed:
 - Bring back index creation in journal database
+- Fix habit success indicator width
 
 ## [0.8.176] - 2022-10-16
 ### Fixed:
--- lib/widgets/charts/dashboard_habits_chart.dart
+++ lib/widgets/charts/dashboard_habits_chart.dart
@@ -1,4 +1,5 @@
 import 'dart:core';
+import 'dart:math';
 
 import 'package:beamer/beamer.dart';
 import 'package:collection/collection.dart';
@@ -324,13 +325,19 @@ class _HabitChartLineState extends State<HabitChartLine> {
                     children: [
                       const SizedBox(width: 70),
                       ...results.map((res) {
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(2),
-                          child: Container(
-                            height: 25,
-                            width: (MediaQuery.of(context).size.width - 300) /
-                                days,
-                            color: colorFromCssHex(res.hexColor),
+                        return Flexible(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(2),
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                left: min(
+                                  MediaQuery.of(context).size.width / 6 / days,
+                                  8,
+                                ),
+                              ),
+                              height: 25,
+                              color: colorFromCssHex(res.hexColor),
+                            ),
                           ),
                         );
                       }),
