d2bdf018ce96ed3e24c0c3225cd4c2cd327a1494
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2022-12-04 21:15:37 +0100

    Comment out habit autocomplete editor experiments

--- lib/pages/settings/habits/habit_details_page.dart
+++ lib/pages/settings/habits/habit_details_page.dart
@@ -15,7 +15,6 @@ import 'package:lotti/themes/theme.dart';
 import 'package:lotti/widgets/app_bar/title_app_bar.dart';
 import 'package:lotti/widgets/form_builder/cupertino_datepicker.dart';
 import 'package:lotti/widgets/journal/entry_tools.dart';
-import 'package:lotti/widgets/settings/habits/habit_autocomplete_widget.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HabitDetailsPage extends StatelessWidget {
@@ -205,7 +204,7 @@ class HabitDetailsPage extends StatelessWidget {
                         ],
                       ),
                     ),
-                    const HabitAutocompleteWrapper(),
+                    // const HabitAutocompleteWrapper(),
                   ],
                 ),
               ),
