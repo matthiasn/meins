6e276b62daf587dc6c0b9a5c349d5d79df47c159
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-01-08 14:22:10 +0100

    Use entities cache in measurement summary

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Wire full-text database search (no refresh yet)
 - Add tags in full-text search
 - Add entities cache for faster lookup of measurable data types
+- Use entities cache in measurement summary
 
 ## [0.8.227] - 2023-01-07
 ### Changed:
--- lib/widgets/journal/entry_details/measurement_summary.dart
+++ lib/widgets/journal/entry_details/measurement_summary.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lotti/classes/entity_definitions.dart';
 import 'package:lotti/classes/journal_entities.dart';
-import 'package:lotti/database/database.dart';
 import 'package:lotti/get_it.dart';
+import 'package:lotti/services/entities_cache_service.dart';
 import 'package:lotti/widgets/charts/dashboard_measurables_chart.dart';
 import 'package:lotti/widgets/charts/utils.dart';
 import 'package:lotti/widgets/journal/entry_tools.dart';
@@ -10,13 +9,12 @@ import 'package:lotti/widgets/journal/helpers.dart';
 import 'package:lotti/widgets/journal/text_viewer_widget.dart';
 
 class MeasurementSummary extends StatelessWidget {
-  MeasurementSummary(
+  const MeasurementSummary(
     this.measurementEntry, {
     this.showChart = true,
     super.key,
   });
 
-  final JournalDb _db = getIt<JournalDb>();
   final MeasurementEntry measurementEntry;
   final bool showChart;
 
@@ -24,44 +22,37 @@ class MeasurementSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     final data = measurementEntry.data;
 
-    return StreamBuilder<MeasurableDataType?>(
-      stream: _db.watchMeasurableDataTypeById(data.dataTypeId),
-      builder: (
-        BuildContext context,
-        AsyncSnapshot<MeasurableDataType?> typeSnapshot,
-      ) {
-        final dataType = typeSnapshot.data;
+    final dataType =
+        getIt<EntitiesCacheService>().getDataTypeById(data.dataTypeId);
 
-        if (dataType == null) {
-          return const SizedBox.shrink();
-        }
+    if (dataType == null) {
+      return const SizedBox.shrink();
+    }
 
-        return Padding(
-          padding: const EdgeInsets.only(top: 5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (measurementEntry.entryText?.plainText != null && !showChart)
-                TextViewerWidget(
-                  entryText: measurementEntry.entryText,
-                  maxHeight: 120,
-                ),
-              if (showChart)
-                DashboardMeasurablesChart(
-                  dashboardId: null,
-                  rangeStart: getRangeStart(context: context),
-                  rangeEnd: getRangeEnd(),
-                  measurableDataTypeId: measurementEntry.data.dataTypeId,
-                ),
-              const SizedBox(height: 8),
-              EntryTextWidget(
-                entryTextForMeasurable(data, dataType),
-                padding: EdgeInsets.zero,
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (measurementEntry.entryText?.plainText != null && !showChart)
+            TextViewerWidget(
+              entryText: measurementEntry.entryText,
+              maxHeight: 120,
+            ),
+          if (showChart)
+            DashboardMeasurablesChart(
+              dashboardId: null,
+              rangeStart: getRangeStart(context: context),
+              rangeEnd: getRangeEnd(),
+              measurableDataTypeId: measurementEntry.data.dataTypeId,
+            ),
+          const SizedBox(height: 8),
+          EntryTextWidget(
+            entryTextForMeasurable(data, dataType),
+            padding: EdgeInsets.zero,
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
