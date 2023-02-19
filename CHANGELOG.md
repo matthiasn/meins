242aefbc189072ce3a60a3cd3bdb538cbddfabbb
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-02-19 20:36:05 +0100

    Revert save button color

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Fixed:
 - Habit fail button color
+- Save button color
 
 ## [0.8.267] - 2023-02-19
 ### Changed:
--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -248,7 +248,7 @@ const taskFormFieldStyle = TextStyle(color: Colors.black87);
 TextStyle saveButtonStyle() => TextStyle(
       fontSize: fontSizeMedium,
       fontWeight: FontWeight.bold,
-      color: styleConfig().primaryColor.darken(25),
+      color: styleConfig().alarm.darken(),
     );
 
 TextStyle failButtonStyle() => TextStyle(
