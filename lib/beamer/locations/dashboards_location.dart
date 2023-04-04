c95ca1134097cb8f0af0b877bf9990efc60177ef
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-04-05 00:25:30 +0200

    Refactoring: remove habit dialog from dashboards

--- lib/beamer/locations/dashboards_location.dart
+++ lib/beamer/locations/dashboards_location.dart
@@ -1,10 +1,7 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'package:lotti/beamer/beamer_delegates.dart';
-import 'package:lotti/pages/create/complete_habit_dialog.dart';
 import 'package:lotti/pages/dashboards/dashboard_page.dart';
 import 'package:lotti/pages/dashboards/dashboards_list_page.dart';
-import 'package:lotti/themes/theme.dart';
 import 'package:lotti/utils/uuid.dart';
 
 class DashboardsLocation extends BeamLocation<BeamState> {
@@ -14,14 +11,11 @@ class DashboardsLocation extends BeamLocation<BeamState> {
   List<String> get pathPatterns => [
         '/dashboards',
         '/dashboards/:dashboardId',
-        '/dashboards/:dashboardId/complete_habit/:habitId',
       ];
 
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
-    bool pathContains(String s) => state.uri.path.contains(s);
     final dashboardId = state.pathParameters['dashboardId'];
-    final habitId = state.pathParameters['habitId'];
 
     final pages = [
       const BeamPage(
@@ -34,32 +28,6 @@ class DashboardsLocation extends BeamLocation<BeamState> {
         BeamPage(
           key: ValueKey('dashboards-$dashboardId'),
           child: DashboardPage(dashboardId: dashboardId!),
-        ),
-      if ((isUuid(dashboardId) || pathContains('carousel')) &&
-          habitId != null &&
-          isUuid(habitId))
-        BeamPage(
-          routeBuilder: (
-            BuildContext context,
-            RouteSettings settings,
-            Widget child,
-          ) {
-            return DialogRoute<void>(
-              context: context,
-              builder: (context) => child,
-              settings: settings,
-              barrierColor: styleConfig().negspace.withOpacity(0.54),
-            );
-          },
-          key: ValueKey('dashboards-habit-$habitId'),
-          child: HabitDialog(
-            habitId: habitId,
-            beamerDelegate: dashboardsBeamerDelegate,
-          ),
-          onPopPage: (context, delegate, _, page) {
-            dashboardsBeamerDelegate.beamBack();
-            return false;
-          },
         ),
     ];
 
