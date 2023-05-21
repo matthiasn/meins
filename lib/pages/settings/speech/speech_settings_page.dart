e3985c1bea16dccbe8b97c708ecf28957dd54ed9
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-05-21 23:37:29 +0200

    Refactor: remove unnecessary Scaffold

--- lib/pages/settings/speech/speech_settings_page.dart
+++ lib/pages/settings/speech/speech_settings_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lotti/blocs/settings/speech/speech_settings_cubit.dart';
 import 'package:lotti/blocs/settings/speech/speech_settings_state.dart';
 import 'package:lotti/pages/settings/sliver_box_adapter_page.dart';
-import 'package:lotti/themes/theme.dart';
 import 'package:lotti/widgets/settings/speech/whisper_model_card.dart';
 
 class SpeechSettingsPage extends StatelessWidget {
@@ -16,21 +15,18 @@ class SpeechSettingsPage extends StatelessWidget {
 
     return BlocProvider<SpeechSettingsCubit>(
       create: (BuildContext context) => SpeechSettingsCubit(),
-      child: Scaffold(
-        backgroundColor: styleConfig().negspace,
-        body: BlocBuilder<SpeechSettingsCubit, SpeechSettingsState>(
-          builder: (context, snapshot) {
-            return SliverBoxAdapterPage(
-              title: localizations.settingsSpeechTitle,
-              showBackButton: true,
-              child: Column(
-                children: [
-                  ...snapshot.availableModels.map(WhisperModelCard.new),
-                ],
-              ),
-            );
-          },
-        ),
+      child: BlocBuilder<SpeechSettingsCubit, SpeechSettingsState>(
+        builder: (context, snapshot) {
+          return SliverBoxAdapterPage(
+            title: localizations.settingsSpeechTitle,
+            showBackButton: true,
+            child: Column(
+              children: [
+                ...snapshot.availableModels.map(WhisperModelCard.new),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
