0a1c97ffb196181fc99d1a84455a922c740f446f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-05-31 13:51:17 +0200

    Bring back semantics labels & fix Fluttium test

--- lib/widgets/habits/status_segmented_control.dart
+++ lib/widgets/habits/status_segmented_control.dart
@@ -26,21 +26,25 @@ class HabitStatusSegmentedControl extends StatelessWidget {
           value: HabitDisplayFilter.openNow,
           selected: filter,
           label: localizations.habitsFilterOpenNow,
+          semanticsLabel: 'Habits - due',
         ),
         buttonSegment(
           value: HabitDisplayFilter.pendingLater,
           selected: filter,
           label: localizations.habitsFilterPendingLater,
+          semanticsLabel: 'Habits - later',
         ),
         buttonSegment(
           value: HabitDisplayFilter.completed,
           selected: filter,
           label: localizations.habitsFilterCompleted,
+          semanticsLabel: 'Habits - done',
         ),
         buttonSegment(
           value: HabitDisplayFilter.all,
           selected: filter,
           label: localizations.habitsFilterAll,
+          semanticsLabel: 'Habits - all',
         ),
       ],
     );
@@ -51,11 +55,13 @@ ButtonSegment<HabitDisplayFilter> buttonSegment({
   required HabitDisplayFilter value,
   required HabitDisplayFilter selected,
   required String label,
+  required String semanticsLabel,
 }) {
   return ButtonSegment(
     value: value,
     label: Text(
       label,
+      semanticsLabel: semanticsLabel,
       style: value == selected
           ? buttonLabelStyle().copyWith(color: Colors.black)
           : buttonLabelStyle()
